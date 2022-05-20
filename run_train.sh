0983f6f0264a2c8297e47669c36216a92d13ad79
Sebastian Jaszczur <sebastian.jaszczur@gmail.com>
2022-05-20 19:28:39 +0200

    Further BERT training

--- run_train.sh
+++ run_train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --job-name=lizard_train
+#SBATCH --job-name=lizard_train3
 #SBATCH --partition=common
 #SBATCH --qos=16gpu3d
-#SBATCH --gres=gpu:1
-#SBATCH --time=0-05:00:00
-#SBATCH --output=/home/sj359674/sbatchlogs.txt
+#SBATCH --gres=gpu:rtx2080ti:1
+#SBATCH --time=0-10:00:00
+#SBATCH --output=/home/sj359674/sbatchlogs5.txt
 
 source venv/bin/activate
 python3 bert_train.py
