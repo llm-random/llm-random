db00998cee8c48c5ebd120129349b202ba225c67
Kuba <kuba@pop-os.localdomain>
2022-10-15 15:34:27 +0200

    Add pruner

--- research/reinitialization/test_linears.py
+++ research/reinitialization/test_linears.py
@@ -3,23 +3,10 @@ import torch
 from research.reinitialization import linears
 
 from lizrd.support.test_utils import GeneralTestCase
+from research.reinitialization.pruner import Pruner
 
 
-class PruneLinearCase(GeneralTestCase):
-    def _test_prune(self, layer, bias_tensor, input_tensor):
-        # prune with probability 0
-        res = layer(input_tensor)
-        layer.prune_unstr(0)
-        res_after_prune = layer(input_tensor)
-        self.assertTensorEqual(res, res_after_prune)
-
-        # prune with probability 1
-        layer.prune_unstr(1)
-        res_after_prune = layer(input_tensor)
-        self.assertTensorEqual(res_after_prune, bias_tensor.repeat(input_tensor.shape[0], 1))
-
-
-class TestReinitLinear(PruneLinearCase):
+class TestReinitLinear(GeneralTestCase):
     def test_smoke(self):
         shapes = [(1, 5), (10, 3), (3, 3)]
         types = [torch.float32, torch.float64, torch.double]
@@ -32,6 +19,18 @@ class TestReinitLinear(PruneLinearCase):
         for shape, dtype, device in zip(shapes, types, devices):
             linears.ReinitLinear(*shape, dtype=dtype, device=device)
 
+    def _test_prune(self, layer, bias_tensor, input_tensor):
+        # prune with probability 0
+        res = layer(input_tensor)
+        layer.prune_unstr(0)
+        res_after_prune = layer(input_tensor)
+        self.assertTensorEqual(res, res_after_prune)
+
+        # prune with probability 1
+        layer.prune_unstr(1)
+        res_after_prune = layer(input_tensor)
+        self.assertTensorEqual(res_after_prune, bias_tensor.repeat(input_tensor.shape[0], 1))
+
     def test_basic(self):
         layer = linears.ReinitLinear(2, 5)
         b = layer.bias.data
@@ -40,11 +39,12 @@ class TestReinitLinear(PruneLinearCase):
         self._test_prune(layer, b, t)
 
 
-class TestReinitFF(PruneLinearCase):
+class TestReinitFF(GeneralTestCase):
     def test_smoke(self):
-        linears.ReinitFF(10, 2)
-        linears.ReinitFF(10, 1)
-        linears.ReinitFF(5, 5)
+        pruner = Pruner(1, 0.5)
+        linears.ReinitFF(10, 2, pruner)
+        linears.ReinitFF(10, 1, pruner)
+        linears.ReinitFF(5, 5, pruner)
 
     def test_basic(self):
         layer = linears.ReinitFF(10, 2)
@@ -52,3 +52,23 @@ class TestReinitFF(PruneLinearCase):
         t = torch.rand((10, 10))
 
         self._test_prune(layer, b, t)
+
+    def test_with_pruner(self):
+        layer = layer = linears.ReinitFF(10, 2)
+
+        b = layer.linears[2].bias.data
+        t = torch.rand((10, 10))
+
+    # TODO: change it, pruner works differently
+    def _test_prune(self, layer, bias_tensor, input_tensor, pruner):
+        # prune with probability 0
+        res = layer(input_tensor)
+        layer.prune_unstr(0)
+        res_after_prune = layer(input_tensor)
+        self.assertTensorEqual(res, res_after_prune)
+
+        # prune with probability 1
+        layer.prune_unstr(1)
+        res_after_prune = layer(input_tensor)
+        self.assertTensorEqual(res_after_prune, bias_tensor.repeat(input_tensor.shape[0], 1))
+        
