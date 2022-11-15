62a028777c2e8ff6f08bb9ee86badbeec1f871fd
Maciej Pióro <m@ciekpio.ro>
2022-11-15 11:57:08 +0100

    Fix reinit syntax

--- research/reinitialization/train/reinit_train.py
+++ research/reinitialization/train/reinit_train.py
@@ -33,7 +33,7 @@ parser.add_argument("--pruner_n_steps", type=int, default=None)
 parser.add_argument("--project_name", type=str)
 
 parser.add_argument("--ff_layer", type=str, default="regular")
-parser.add_argument("--learning_rate", type=float, default=1=8e-4)
+parser.add_argument("--learning_rate", type=float, default=8e-4)
 parser.add_argument("--mask_loss_weight", type=float, default=1.0)
 parser.add_argument("--class_loss_weight", type=float, default=1.0)
 parser.add_argument("--mask_percent", type=float, default=0.15)
