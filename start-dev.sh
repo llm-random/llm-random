b2be5d67e6b162d54ce14590cb5510270cd58096
Maciej Pióro <m@ciekpio.ro>
2023-02-09 10:05:54 +0100

    Remove datasets setup from start-dev

--- start-dev.sh
+++ start-dev.sh
@@ -9,7 +9,6 @@ if [ ! -d "venv" ]; then
     . venv/bin/activate
     python3 -m pip install -U pip
     python3 -m pip install -r requirements.txt
-    pip install --upgrade --force-reinstall datasets==2.9.0
 else
     echo "[start-dev.sh] I don't know Walt, seems kind of sus to me. You've already got a venv folder, do you really want to make another one? Try again."
 fi
