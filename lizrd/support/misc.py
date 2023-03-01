274a77a9c4f383ad2347bf0f1565deb3d33c011a
Szymon Antoniak <74513955+Simontwice@users.noreply.github.com>
2023-03-01 15:33:58 +0100

    Update misc.py
    rename variables in parameter counting

--- lizrd/support/misc.py
+++ lizrd/support/misc.py
@@ -13,7 +13,7 @@ def make_concise_datetime() -> str:
 
 def count_parameters(model, args, VOCAB_SIZE):
     model_n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-    embedding_params = 2 * VOCAB_SIZE * args.dm
-    last_layer_params = args.cutoff * args.dm
-    model_n_params -= embedding_params + last_layer_params
+    input_embedding_and_head_params = 2 * VOCAB_SIZE * args.dm
+    pos_embedding_params = args.cutoff * args.dm
+    model_n_params -= input_embedding_and_head_params + pos_embedding_params
     return model_n_params
