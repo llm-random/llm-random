318a691c4f432ea8a3ebc008ecbaee9376dbf170
Simontwice <szymon.antoniak1@gmail.com>
2023-04-20 15:15:05 +0200

    prune legacy hyperparameters

--- research/conditional/train/utils.py
+++ research/conditional/train/utils.py
@@ -26,33 +26,17 @@ def introduce_parser_arguments(parser):
     # parameters usually changed for experiments
 
     parser.add_argument("--ff_mode", type=str, default="vanilla")
+    parser.add_argument("--attention_mode", type=str, default="vanilla")
     parser.add_argument(
         "--project_name", type=str, default="nonlinearities/initial_tests"
     )
     parser.add_argument("--name", type=str, default="")
     parser.add_argument("--learning_rate", type=float, default=5e-5)
     parser.add_argument("--learning_rate_ff", type=float)
-    parser.add_argument("--neck_width_increase_ratio", type=float, default=1.0)
-    parser.add_argument("--hack_for_batch_size", action="store_true")
-    parser.add_argument("--deterministic", action="store_true")
     parser.add_argument("--gradient_checkpointing", action="store_true")
 
     # experimental/legacy parameters
 
-    parser.add_argument("--n_chunks", type=int, default=1)
-    parser.add_argument("--exp_rate", type=float, default=4.0)
-    parser.add_argument("--bottleneck_size", type=int, default=4)
-
-    parser.add_argument("--n_ff_heads", type=int, default=4)
-    parser.add_argument("--d_ff_head", type=int, default=256)
-    parser.add_argument("--multineck_mode", type=str, default="none")
-    parser.add_argument("--inception_head_sizes", nargs="*", type=float)
-    parser.add_argument("--dbottle", type=int, default=-1)
-    parser.add_argument("--n_bias_copies", type=int, default=-1)
-    parser.add_argument("--attention_mode", type=str, default="vanilla")
-    parser.add_argument("--attention_thinning_coeff", type=float, default=1.0)
-    parser.add_argument("--n_steps_eval", type=int, default=100)
-    parser.add_argument("--class_loss_weight", type=float, default=1.0)
     parser.add_argument("--save_model_checkpoints", action="store_true")
     parser.add_argument("--x_flop", action="store_true")
     parser.add_argument("--x_logarithmic", action="store_true")
