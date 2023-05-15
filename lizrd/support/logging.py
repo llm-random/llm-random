755968b3573b7df163540faf08cb9d479d28bbcf
Simontwice <szymon.antoniak1@gmail.com>
2023-05-15 17:41:58 +0200

    unify info passed to logging

--- lizrd/support/logging.py
+++ lizrd/support/logging.py
@@ -217,14 +217,14 @@ class NeptuneLogger(AbstractLogger):
 
     def report_generic_info(self, *, type: str, title: str, iteration: int, **kwargs):
         if type == "scalar":
-            value = kwargs.pop("value")
+            value = kwargs.pop("data")
             series = kwargs.pop("series", None)
             self.report_scalar(
                 title=title, value=value, iteration=iteration, series=series
             )
 
         elif type == "plotly":
-            figure = kwargs.pop("figure")
+            figure = kwargs.pop("data")
             series = kwargs.pop("series", None)
             self.report_plotly(
                 figure=figure, title=title, iteration=iteration, series=series
