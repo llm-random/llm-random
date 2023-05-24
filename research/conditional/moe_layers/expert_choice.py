e29b744ba29913f22cfebcd47a2c3be7df3a5fe8
Kuba Krajewski <gim.jakubk@gmail.com>
2023-05-24 23:54:55 +0200

    Next debug

--- research/conditional/moe_layers/expert_choice.py
+++ research/conditional/moe_layers/expert_choice.py
@@ -34,7 +34,7 @@ class ExpertChoiceFF(nn.Module):
             get_init_weight((n_experts, expert_size, dmodel), fan_in=expert_size)
         )
         self.gate = nn.Parameter(
-            get_init_weight((cutoff, n_experts), fan_in=dmodel)
+            get_init_weight((dmodel, n_experts), fan_in=dmodel)
         ).requires_grad_(True)
         self.old_gate = self.gate.data.clone()
 
@@ -48,10 +48,9 @@ class ExpertChoiceFF(nn.Module):
         # x is (batch, cutoff, dmodel)
         batch_size, cutoff = x.shape[0], x.shape[1]
 
-        # transform cutoff to n_experts
         # expert embedding
         gate_out = einsum(
-            "batch_size cutoff dmodel, cutoff n_experts -> batch_size cutoff n_experts",
+            "batch_size cutoff dmodel, dmodel n_experts -> batch_size cutoff n_experts",
             x,
             self.gate,
         )
@@ -71,16 +70,15 @@ class ExpertChoiceFF(nn.Module):
         x_before_ff = x
 
         # choose the right tokens from x for each expert
-        # x = torch.index_select(x, dim=0, index=topk_indices.flatten()).reshape(
-        #     (self.n_experts, self.topk, self.dmodel)
-        # )
+        x = torch.index_select(x, dim=0, index=topk_indices.flatten()).reshape(
+            (self.n_experts, self.topk, self.dmodel)
+        )
         x = x.reshape(
             (self.n_experts, self.topk, self.dmodel)
         )
-        ash.assert_shape("e k m", x, e=self.n_experts, k=self.topk, m=self.dmodel)
 
         # feed through ff
-        # lin1 maps from (n_experts, topk, dmodel) to (n_experts, topk, dff/n_experts)
+        # lin1 maps from (n_experts, topk, dmodel) to (n_experts, topk, exp_size)
         x = einsum(
             "n_exp topk dmodel, n_exp dmodel exp_size -> n_exp topk exp_size",
             x,
@@ -89,7 +87,7 @@ class ExpertChoiceFF(nn.Module):
 
         x = F.relu(x)
 
-        # lin2 maps from (...) to (n_experts, topk, dmodel)
+        # lin2 maps from (n_experts, topk, exp_size) to (n_experts, topk, dmodel)
         x = einsum(
             "n_exp topk exp_size, n_exp exp_size dmodel -> n_exp topk dmodel",
             x,
@@ -98,18 +96,17 @@ class ExpertChoiceFF(nn.Module):
         ash.assert_shape("e k m", x, e=self.n_experts, k=self.topk, m=self.dmodel)
 
         # multiply by softmax
-        # ash.assert_shape("e k", topk_values, e=self.n_experts, k=self.topk)
-        # x = einsum("n_exp topk dmodel, n_exp topk -> n_exp topk dmodel", x, topk_values)
+        ash.assert_shape("e k", topk_values, e=self.n_experts, k=self.topk)
+        x = einsum("n_exp topk dmodel, n_exp topk -> n_exp topk dmodel", x, topk_values)
 
         # flatten x s. t. first dimension is tokens instead of n_experts x topk
         x = x.flatten(start_dim=0, end_dim=1)
 
         # add tokens that have been processed by more than one expert
-        # z = torch.zeros_like(x_before_ff).type(x.type())
-        # z.index_add_(dim=0, index=topk_indices.flatten().to(int), source=x)
+        z = torch.zeros_like(x_before_ff).type(x.type())
+        z.index_add_(dim=0, index=topk_indices.flatten().to(int), source=x)
 
         # reshape to (batch_size, cutoff, dmodel)
-        # x = z.reshape((batch_size, cutoff, self.dmodel))
-        x = x.reshape((batch_size, cutoff, self.dmodel))
+        x = z.reshape((batch_size, cutoff, self.dmodel))
 
         return x
