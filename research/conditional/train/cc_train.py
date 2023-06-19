06665c9eb5c8ab442177b108515f394759036c3d
Kuba Krajewski <gim.jakubk@gmail.com>
2023-06-20 00:40:24 +0200

    debugging device id

--- research/conditional/train/cc_train.py
+++ research/conditional/train/cc_train.py
@@ -30,59 +30,61 @@ def main(rank: Optional[int]):
         torch.cuda.set_device(rank)
 
     VOCAB_SIZE = 30522 if args.model_type == "bert" else 50257
-    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    id = rank if rank is not None else 0
+    DEVICE = torch.device(f"cuda:{id}" if torch.cuda.is_available() else "cpu")
     misc.print_available_gpus()
 
-    distributed = True if rank is not None else False
-    train_dataloader = get_processed_dataset(
-        max_total_length=args.cutoff,
-        mask_percent=args.mask_percent,
-        device=DEVICE,
-        num_workers=args.num_workers,
-        batch_size=args.batch_size,
-        seed=args.data_seed,
-        model_type=args.model_type,
-        distributed=distributed,
-    )
+    # distributed = True if rank is not None else False
+    # train_dataloader = get_processed_dataset(
+    #     max_total_length=args.cutoff,
+    #     mask_percent=args.mask_percent,
+    #     device=DEVICE,
+    #     num_workers=args.num_workers,
+    #     batch_size=args.batch_size,
+    #     seed=args.data_seed,
+    #     model_type=args.model_type,
+    #     distributed=distributed,
+    # )
 
-    ff_layer_fun = get_ff_layer(args)
-    attention_layer_fun = get_attention_layer(args)
+    # ff_layer_fun = get_ff_layer(args)
+    # attention_layer_fun = get_attention_layer(args)
 
-    model = get_model(
-        max_length=args.cutoff,
-        vocab_size=VOCAB_SIZE,
-        ff_layer_fun=ff_layer_fun,
-        attention_layer_fun=attention_layer_fun,
-        dm=args.dmodel,
-        n_blocks=args.n_blocks,
-        device=DEVICE,
-        gradient_checkpointing=args.gradient_checkpointing,
-    )
+    # model = get_model(
+    #     max_length=args.cutoff,
+    #     vocab_size=VOCAB_SIZE,
+    #     ff_layer_fun=ff_layer_fun,
+    #     attention_layer_fun=attention_layer_fun,
+    #     dm=args.dmodel,
+    #     n_blocks=args.n_blocks,
+    #     device=DEVICE,
+    #     gradient_checkpointing=args.gradient_checkpointing,
+    # )
 
-    # make model distributed if necessary
-    if rank is not None:
-        model = DDP(model, device_ids=[rank])
+    # # make model distributed if necessary
+    # if rank is not None:
+    #     model = DDP(model, device_ids=[rank])
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
-    logger = get_logger(args, model, VOCAB_SIZE)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    # logger = get_logger(args, model, VOCAB_SIZE)
 
-    trainer = ConditionalTrainer(
-        model=model,
-        optimizer=optimizer,
-        train_dataloader=train_dataloader,
-        batch_size=args.batch_size,
-        vocab_size=VOCAB_SIZE,
-        mask_percent=args.mask_percent,
-        mixed_precision=args.mixed_precision,
-        logger=logger,
-        hack_for_batch_size=args.hack_for_batch_size,
-        model_type=args.model_type,
-        logging_interval_loss=args.logging_interval_loss,
-        logging_interval_light=args.logging_interval_light,
-        logging_interval_heavy=args.logging_interval_heavy,
-        gpu_id=rank,
-    )
-    trainer.train(args.n_steps)
+    # trainer = ConditionalTrainer(
+    #     model=model,
+    #     optimizer=optimizer,
+    #     train_dataloader=train_dataloader,
+    #     batch_size=args.batch_size,
+    #     vocab_size=VOCAB_SIZE,
+    #     mask_percent=args.mask_percent,
+    #     mixed_precision=args.mixed_precision,
+    #     logger=logger,
+    #     hack_for_batch_size=args.hack_for_batch_size,
+    #     model_type=args.model_type,
+    #     logging_interval_loss=args.logging_interval_loss,
+    #     logging_interval_light=args.logging_interval_light,
+    #     logging_interval_heavy=args.logging_interval_heavy,
+    #     gpu_id=rank,
+    # )
+    # trainer.train(args.n_steps)
 
     if rank is not None:
         destroy_process_group()
