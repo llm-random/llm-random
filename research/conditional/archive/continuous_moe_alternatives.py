c619152b4fb889e4831e5552b71dc8f811b4b039
Simontwice <szymon.antoniak1@gmail.com>
2023-07-03 17:02:14 +0200

    add logging list of scalars introduce flop matched continuous moe for ease of comparison with vanilla transformers intrudoce new versions of cont moe

--- research/conditional/archive/continuous_moe_alternatives.py
+++ research/conditional/archive/continuous_moe_alternatives.py
@@ -1,10 +1,13 @@
 import dataclasses
 
+import numpy as np
 import torch
+from plotly import express as px
 
 from lizrd.core import misc, nn
 from lizrd.support import ash
-from research.conditional.archive.rogue_code import set_highest_index_one
+from lizrd.support.logging import make_histogram
+from research.conditional.archive.rogue_code import set_highest_index_one, entropy
 from research.conditional.moe_layers.continuous_moe import (
     stable_softmax_temperature,
     ContinuousMoeBaseClass,
@@ -130,6 +133,112 @@ class ContinuousMoEQuickNosoftmax(ContinuousMoeBaseClass):
         emit_weights = merge_weights
         return merge_weights, emit_weights
 
+    def merge_map_emit(self, x, merge_weights, emit_weights):
+        x = misc.einsum(
+            "B S c d, B S e c-> B S e d",
+            x,
+            merge_weights,
+            use_opt_einsum=self.use_opt_einsum,
+        )
+        x = self.layernorm1(x)
+        x = misc.einsum(
+            "B S e d, d e f -> B S e f",
+            x,
+            self.lin1,
+            use_opt_einsum=self.use_opt_einsum,
+        )
+        x = torch.relu_(x)
+        x = misc.einsum(
+            "B S e f, d e f -> B S e d",
+            x,
+            self.lin2,
+            use_opt_einsum=self.use_opt_einsum,
+        )
+        x = self.layernorm2(x)
+        x = misc.einsum(
+            "B S e d, B S e c -> B S c d",
+            x,
+            emit_weights,
+            use_opt_einsum=self.use_opt_einsum,
+        )
+        return x
+
+    def init_parameters(self):
+        # lin1 is parameter, one dimension for experts of size dmodel to dff/n_experts
+        self.lin1 = nn.Parameter(
+            misc.get_init_weight(
+                (self.dm, self.n_experts, self.expert_size), fan_in=self.dm
+            )
+        )
+
+        self.lin2 = nn.Parameter(
+            misc.get_init_weight(
+                (self.dm, self.n_experts, self.expert_size), fan_in=self.expert_size
+            )
+        )
+        # controller: send a token of size dmodel to n_experts scalars
+        self.controller = nn.Parameter(
+            misc.get_init_weight((self.dm, self.n_experts), fan_in=self.dm)
+        )
+        self.layernorm1 = nn.LayerNorm(self.dm)
+        self.layernorm2 = nn.LayerNorm(self.dm)
+
+    def log_heavy(self):
+        return {}
+
+
+@ash.check("... dinp -> ... dout")
+class ContinuousMoELayernorm(ContinuousMoeBaseClass):
+    def merge_map_emit(self, x, merge_weights, emit_weights):
+        x = misc.einsum(
+            "B S c d, B S e c-> B S e d",
+            x,
+            merge_weights,
+            use_opt_einsum=self.use_opt_einsum,
+        )
+        x = self.layernorm1(x)
+        x = misc.einsum(
+            "B S e d, d e f -> B S e f",
+            x,
+            self.lin1,
+            use_opt_einsum=self.use_opt_einsum,
+        )
+        x = torch.relu_(x)
+        x = misc.einsum(
+            "B S e f, d e f -> B S e d",
+            x,
+            self.lin2,
+            use_opt_einsum=self.use_opt_einsum,
+        )
+        x = self.layernorm2(x)
+        x = misc.einsum(
+            "B S e d, B S e c -> B S c d",
+            x,
+            emit_weights,
+            use_opt_einsum=self.use_opt_einsum,
+        )
+        return x
+
+    def init_parameters(self):
+        # lin1 is parameter, one dimension for experts of size dmodel to dff/n_experts
+        self.lin1 = nn.Parameter(
+            misc.get_init_weight(
+                (self.dm, self.n_experts, self.expert_size), fan_in=self.dm
+            )
+        )
+
+        self.lin2 = nn.Parameter(
+            misc.get_init_weight(
+                (self.dm, self.n_experts, self.expert_size), fan_in=self.expert_size
+            )
+        )
+        # controller: send a token of size dmodel to n_experts scalars
+        self.controller = nn.Parameter(
+            misc.get_init_weight((self.dm, self.n_experts), fan_in=self.dm)
+        )
+        self.layernorm1 = nn.LayerNorm(self.dm)
+        self.layernorm2 = nn.LayerNorm(self.dm)
+
     def log_heavy(self):
         return {}
 
@@ -151,7 +260,7 @@ class ContinuousMoEQuickAdaTemp(ContinuousMoeBaseClass):
         merge_logits /= self.temperature_merge
         merge_weights = stable_softmax_temperature(merge_logits, self.temperature_merge)
         self.cache("merge_weights", merge_weights)
-        emit_logits = misc.einsum("B S c d, d e -> B S e c", x, self.controller_emit)
+        emit_logits = misc.einsum("B S c d, d e -> B S e c", x, self.controller_merge)
         emit_logits /= self.temperature_emit
         self.cache("emit_logits", emit_logits)
         emit_weights = stable_softmax_temperature(emit_logits, self.temperature_emit)
@@ -168,11 +277,111 @@ class ContinuousMoEQuickAdaTemp(ContinuousMoeBaseClass):
                 self.temperature_merge = nn.Parameter(torch.ones(1))
         else:
             if self.share_by_emit_merge:
-                self.temperature_emit = nn.Parameter(torch.ones(self.n_experts))
+                self.temperature_emit = nn.Parameter(torch.ones(self.n_experts, 1))
                 self.temperature_merge = self.temperature_emit
             else:
-                self.temperature_emit = nn.Parameter(torch.ones(self.n_experts))
-                self.temperature_merge = nn.Parameter(torch.ones(self.n_experts))
+                self.temperature_emit = nn.Parameter(torch.ones(self.n_experts, 1))
+                self.temperature_merge = nn.Parameter(torch.ones(self.n_experts, 1))
+
+        self.lin1 = nn.Parameter(
+            misc.get_init_weight(
+                (self.dm, self.n_experts, self.expert_size), fan_in=self.dm
+            )
+        )
+        self.lin2 = nn.Parameter(
+            misc.get_init_weight(
+                (self.dm, self.n_experts, self.expert_size), fan_in=self.expert_size
+            )
+        )
+        self.controller_merge = nn.Parameter(
+            misc.get_init_weight((self.dm, self.n_experts), fan_in=self.dm)
+        )
+
+    def log_heavy(self):
+        log = {}
+        if self.group_size == 1:
+            return log
+        merge_weights = torch.flatten(
+            self.cached_data["merge_weights"], start_dim=0, end_dim=-2
+        )
+        merge_logits = torch.flatten(
+            self.cached_data["merge_logits"], start_dim=0, end_dim=-2
+        )
+        sample_weight_distros = merge_weights[:5]
+        sample_logits_distros = merge_logits[:5]
+
+        for i, sample in enumerate(sample_weight_distros):
+            sample = sample.sort(descending=True).values
+            sample = sample.tolist()
+            fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
+            log[f"merge_weights/sample_{i}"] = fig
+
+        for i, sample in enumerate(sample_logits_distros):
+            sample = sample.sort(descending=True).values
+            sample = sample.tolist()
+            fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
+            log[f"merge_logits/sample_{i}"] = fig
+
+        ent = entropy(merge_weights)
+        max_entropy = np.log(self.n_experts)
+        normalised_ent = ent / max_entropy
+        log["merge_weights/normalised_entropy"] = make_histogram(
+            normalised_ent, title="merge logits entropy (normalised to [0,1])"
+        )
+
+        log[
+            "merge_weights/merge_temperature"
+        ] = self.temperature_merge.data.flatten().tolist()
+        log[
+            "merge_weights/emit_temperature"
+        ] = self.temperature_emit.data.flatten().tolist()
+
+        return log
+
+
+@dataclasses.dataclass(eq=False, repr=False)
+class ContinuousMoEFinal(ContinuousMoeBaseClass):
+    """
+    learnable temperature,
+    either shared by experts or not,
+    either shared for merge and emit or not
+    """
+
+    share_by_experts: bool = True
+    share_by_emit_merge: bool = True
+
+    def get_merge_and_emit_weights(self, x):
+        merge_logits = misc.einsum(
+            "B S c d, d e -> B S e c", x, self.controller_merge + self.controller_base
+        )
+        self.cache("merge_logits", merge_logits)
+        merge_logits /= self.temperature_merge
+        merge_weights = stable_softmax_temperature(merge_logits, self.temperature_merge)
+        self.cache("merge_weights", merge_weights)
+        emit_logits = misc.einsum(
+            "B S c d, d e -> B S e c", x, self.controller_emit + self.controller_base
+        )
+        emit_logits /= self.temperature_emit
+        self.cache("emit_logits", emit_logits)
+        emit_weights = stable_softmax_temperature(emit_logits, self.temperature_emit)
+        self.cache("emit_weights", emit_weights)
+        return merge_weights, emit_weights
+
+    def init_parameters(self):
+        if self.share_by_experts:
+            if self.share_by_emit_merge:
+                self.temperature_emit = nn.Parameter(torch.ones(1))
+                self.temperature_merge = self.temperature_emit
+            else:
+                self.temperature_emit = nn.Parameter(torch.ones(1))
+                self.temperature_merge = nn.Parameter(torch.ones(1))
+        else:
+            if self.share_by_emit_merge:
+                self.temperature_emit = nn.Parameter(torch.ones(self.n_experts, 1))
+                self.temperature_merge = self.temperature_emit
+            else:
+                self.temperature_emit = nn.Parameter(torch.ones(self.n_experts, 1))
+                self.temperature_merge = nn.Parameter(torch.ones(self.n_experts, 1))
 
         self.lin1 = nn.Parameter(
             misc.get_init_weight(
