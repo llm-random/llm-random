f0b4ac4c21d50217890f3a36b5573bfd63c326f6
Simontwice <szymon.antoniak1@gmail.com>
2023-07-08 14:28:20 +0200

    advanced bb slurm cleanup

--- lizrd/scripts/baby_slurm.sh
+++ lizrd/scripts/baby_slurm.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+
 # $1 refers to tmux session name
 # $2 refers to the tmux socket path
 # $3 refers to the space-separated list of log files
 # $4 refers to the number of machines to reserve
 # $5 refers to training command
 # $6 refers to the log file
+
 echo -e "Running baby_slurm.sh \n\n\n"
 echo "SESSION_NAME=$1"
 echo "TMUX_SOCKET_PATH=$2"
@@ -13,8 +15,29 @@ echo "NUM_MACHINES=$4"
 echo "TRAINING_COMMAND=$5"
 echo "LOG_FILE=$6"
 echo -e "\n\n\nWAITING FOR RESOURCES \n\n\n"
+
 export CUDA_VISIBLE_DEVICES=$(python3 -m lizrd.scripts.reserve_machines --session_name $1 --socket_path $2 --gpu_log_files "$3" --no_machines $4)
+
 echo -e "\n\n\nCUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
+
+# define the cleanup procedure
+cleanup() {
+    echo -e "\n\n\nCLEANING UP \n\n\n"
+    IFS=', ' read -r -a array <<< "$CUDA_VISIBLE_DEVICES"
+    for index in "${array[@]}"
+    do
+        echo "Resetting GPU $index"
+        nvidia-smi --gpu-reset -i $index
+    done
+    echo "Killing all child processes"
+    kill 0
+    echo "Killing tmux session $1"
+    echo "EXITING BABY_SLURM"
+    tmux -S $2 kill-session -t $1
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+# the rest of the script
 $5 2>&1 | tee -a $6
-echo "EXITING BABY_SLURM"
-tmux -S $2 kill-session -t $1
