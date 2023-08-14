Fix loss computation

research/conditional/utils/model_utils.py
@@ -1,5 +1,5 @@
 from functools import partial
-from typing import Literal, Optional
+from typing import Literal
 import torch
 import torch.nn.functional as F
 from torch.utils.checkpoint import checkpoint
@@ -39,19 +39,13 @@ from research.conditional.moe_layers.expert_choice import ExpertChoiceFF
 from research.conditional.moe_layers.ff_timed import FeedForwardTimed
 
 
-def make_loss_function(
-    model: Literal["bert", "gpt"],
-    loss_checkpoint_chungs: int,
-    mask_percentage: Optional[float] = None,
-):
+def make_loss_function(model: Literal["bert", "gpt"], loss_checkpoint_chungs: int):
     if model == "bert":
-        assert mask_percentage is not None, "Mask percentage must be specified for BERT"
         if loss_checkpoint_chungs == 0:
-            return partial(calculate_bert_loss, mask_percent=mask_percentage)
+            return partial(calculate_bert_loss)
         else:
             return partial(
                 chungized_bert_loss,
-                mask_percent=mask_percentage,
                 n_chungs=loss_checkpoint_chungs,
             )
     elif model == "gpt":
@@ -71,9 +65,8 @@ def chungized_llm_loss(
     mixed_precision,
     vocab_size,
     n_chungs,
-    mask_percent,
 ):
-    def make_custom_forward(vocab_size):
+    def make_custom_forward():
         def custom_forward(*inputs):
             with torch.autocast(
                 device_type="cuda", enabled=mixed_precision, dtype=torch.float16
@@ -111,21 +104,19 @@ def chungized_llm_loss(
         chunged_inputs, chunged_non_masked_inputs, chunged_non_masked_masks
     ):
         partial_loss_output, partial_correct_tokens, partial_masked_tokens = checkpoint(
-            make_custom_forward(vocab_size), chunged_input, chunged_gt, chunged_mask
+            make_custom_forward(), chunged_input, chunged_gt, chunged_mask
         )
         num_tokens += partial_loss_output.shape[0]
         total_loss += partial_loss_output.sum()
         total_correct_tokens += partial_correct_tokens
         total_masked_tokens += partial_masked_tokens
-    return total_loss / num_tokens / mask_percent, {
+    return total_loss / num_tokens, {
         "correct_tokens": total_correct_tokens,
         "total_masked_tokens": total_masked_tokens,
     }
 
 
-def chungized_bert_loss(
-    batch, model, mixed_precision, vocab_size, mask_percent, n_chungs
-):
+def chungized_bert_loss(batch, model, mixed_precision, vocab_size, n_chungs):
     return chungized_llm_loss(
         input_tokens=batch.masked_tokens,
         gt_tokens=batch.tokens,
@@ -134,7 +125,6 @@ def chungized_bert_loss(
         mixed_precision=mixed_precision,
         vocab_size=vocab_size,
         n_chungs=n_chungs,
-        mask_percent=mask_percent,
     )
 
 
@@ -147,12 +137,11 @@ def chungized_gpt_loss(batch, model, mixed_precision, vocab_size, n_chungs):
         mixed_precision=mixed_precision,
         vocab_size=vocab_size,
         n_chungs=n_chungs,
-        mask_percent=1.0,
     )
 
 
 def calculate_llm_loss(
-    input_tokens, gt_tokens, mask, model, mixed_precision, vocab_size, mask_percent
+    input_tokens, gt_tokens, mask, model, mixed_precision, vocab_size
 ):
     with torch.autocast(
         device_type="cuda", enabled=mixed_precision, dtype=torch.float16
@@ -168,7 +157,7 @@ def calculate_llm_loss(
         reduction="none",
     )
     mask_loss = mask_loss[mask.reshape(-1) == 1]
-    loss = mask_loss.mean() / mask_percent
+    loss = mask_loss.mean()
 
     correct_tokens = gt_tokens.long() == model_output.argmax(dim=-1)
     correct_tokens = correct_tokens.long().reshape(-1) * mask.reshape(-1)
@@ -189,11 +178,10 @@ def calculate_gpt_loss(batch, model, mixed_precision, vocab_size):
         model=model,
         mixed_precision=mixed_precision,
         vocab_size=vocab_size,
-        mask_percent=1.0,
     )
 
 
-def calculate_bert_loss(batch, model, mixed_precision, vocab_size, mask_percent):
+def calculate_bert_loss(batch, model, mixed_precision, vocab_size):
     return calculate_llm_loss(
         input_tokens=batch.masked_tokens,
         gt_tokens=batch.tokens,
@@ -201,7 +189,6 @@ def calculate_bert_loss(batch, model, mixed_precision, vocab_size, mask_percent)
         model=model,
         mixed_precision=mixed_precision,
         vocab_size=vocab_size,
-        mask_percent=mask_percent,
     )
 
 