5447ba104a667bd828addc0533ff31b3c0f000e6
Kuba Krajewski <gim.jakubk@gmail.com>
2023-08-16 15:28:45 +0200

    reformat

--- lizrd/datasets/processed_batch.py
+++ lizrd/datasets/processed_batch.py
@@ -28,9 +28,7 @@ class ProcessedBatch(ABC):
 class ProcessedGPTBatch(ProcessedBatch):
     def __init__(self, processed_examples):
         super().__init__(processed_examples)
-        self.tokens = torch.tensor(
-            [example.tokens for example in processed_examples]
-        )
+        self.tokens = torch.tensor([example.tokens for example in processed_examples])
         self.target_tokens = torch.tensor(
             [example.target_tokens for example in processed_examples]
         )
