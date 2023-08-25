0cc1c56eb7400422061c65ad126815093d48bf65
Simontwice <szymon.antoniak1@gmail.com>
2023-08-25 19:44:03 +0200

    wip

--- lizrd/support/code_versioning_support.py
+++ lizrd/support/code_versioning_support.py
@@ -1,8 +1,159 @@
 import shutil
 import os
 import subprocess
+from typing import Union
 
 from git import Repo, GitCommandError
+
+
+class CodeVersioningDaemon:
+    def __init__(self, remote_name, remote_url, name_for_branch):
+        self.remote_name: str = remote_name
+        self.remote_url: str = remote_url
+        self.name_for_branch: str = name_for_branch
+        self.original_branch: Union[str, None] = None
+        self.current_branch: Union[str, None] = None
+        self.original_branch_commit_hash: Union[str, None] = None
+
+        self.repo: Repo = Repo(find_git_root())
+        self.revert_status: int = 0
+        self.stash_present: bool = False
+        self.stash_message: Union[str, None] = None
+
+    def version_code(self):
+        """
+        Versions the code by stashing uncommited changes, creating a branch copy that is up to date with main, unstashing them and pushing it to a remote repo.
+        """
+        try:
+            # Record current branch
+            self.original_branch = self.repo.active_branch.name
+            self.original_branch_commit_hash = self.repo.head.object.hexsha
+
+            # reject if there are unpushed commits
+            commits_behind = list(
+                self.repo.iter_commits(f"origin/{self.original_branch}..HEAD")
+            )
+            if len(commits_behind) > 0:
+                raise Exception(
+                    f"Unpushed commits detected. Push them first. Aborting..."
+                )
+
+            self.check_if_cemetery_exists()
+
+            self.stash_if_necessary()
+            self.revert_status = 1
+
+            self.repo.git.checkout(b=self.name_for_branch)
+            self.revert_status = 2
+
+            self.repo.git.checkout("main")
+            self.revert_status = 3
+
+            self.repo.git.pull()
+            self.revert_status = 4
+
+            self.repo.git.checkout(self.name_for_branch)
+            self.revert_status = 5
+
+            self.repo.git.merge("main")
+            self.revert_status = 6
+
+            self.unstash_if_necessary()
+            self.revert_status = 7
+
+            self.repo.git.push(self.remote_name, self.name_for_branch)
+            self.revert_status = 8
+
+            self.repo.git.checkout(self.original_branch)
+            self.revert_status = 9
+
+            self.unstash_if_necessary()
+            self.repo.git.branch("-D", self.name_for_branch)
+
+        except GitCommandError:
+            self.handle_failure()
+            raise Exception("Failed to version code. Aborting...")
+
+    def handle_failure(self):
+        if self.revert_status == 0:
+            pass
+        elif self.revert_status == 1:
+            self.unstash_if_necessary()
+        else:
+            if self.revert_status == 4:
+                self.repo.git.checkout("main", "-f")
+                self.repo.git.reset("--hard", "HEAD")
+            elif self.revert_status == 6:
+                print(
+                    f"[CHANGES NEEDED AT USER'S SIDE] Merging of latest main branch into current branch failed. Please merge manually and try again."
+                )
+            self.clean_up_new_branch()
+            self.reset_to_original_branch_and_commit()
+
+    def clean_up_new_branch(self):
+        self.repo.git.checkout("main", "-f")
+        self.repo.git.branch("-D", self.name_for_branch)
+
+    def reset_to_original_branch_and_commit(self):
+        self.repo.git.checkout(self.original_branch, "-f")
+        self.repo.head.reset(
+            self.original_branch_commit_hash, index=True, working_tree=True
+        )
+        self.unstash_if_necessary()
+
+    def check_if_cemetery_exists(self):
+        for remote in self.repo.remotes:
+            if remote.name == self.remote_name:
+                if remote.url == self.remote_url:
+                    return
+                else:
+                    raise Exception(
+                        f"Wrong url under remote repo {self.remote_name}: {self.repo.remotes[self.remote_name].url.strip()}, should be {self.remote_url}"
+                    )
+        self.repo.create_remote(self.remote_name, url=self.remote_url)
+        return
+
+    def stash_if_necessary(self):
+        if self.repo.is_dirty():
+            self.stash_message = f"versioning_{self.name_for_branch}"
+            try:
+                self.repo.git.stash("save", "--message", self.stash_message)
+                self.stash_present = True
+            except GitCommandError:
+                raise GitCommandError(
+                    "Failed to stash changes. Reverting changes. If anything goes wrong, consult local history. Aborting..."
+                )
+
+    def unstash_if_necessary(self):
+        if self.stash_present:
+            try:
+                stash_id = self.find_stash_by_message(self.stash_message)
+                self.repo.git.stash("apply", stash_id)
+            except subprocess.CalledProcessError as e:
+                print(
+                    "Error encountered while applying stashed changes.",
+                    e,
+                    "Trying to merge, favoring stash...",
+                )
+                try:
+                    subprocess.run(["git", "checkout", "--theirs", "."], check=True)
+                except subprocess.CalledProcessError as e:
+                    print(
+                        "Error encountered while resolving conflicts in favor of stash.",
+                        e,
+                    )
+                    raise GitCommandError(
+                        "Unstashing changes on rebased branch failed; conflicts occurred. Could not resolve them automatically."
+                    )
+
+    def find_stash_by_message(self, stash_message):
+        result = subprocess.run(["git", "stash", "list"], stdout=subprocess.PIPE)
+        stashes = result.stdout.decode("utf-8").split("\n")
+
+        for stash in stashes:
+            if stash_message in stash:
+                return stash.split(":")[0]  # return stash name (ex: stash@{0})
+        raise GitCommandError(f"Could not find stash with message {stash_message}")
 
 
 def version_and_copy_code(
@@ -17,8 +168,10 @@ def version_and_copy_code(
     Prerequisite: the user needs to be able to push to the remote repo from the command line without entering a password.
     If not met, the user needs to set up ssh keys.
     """
+    # Create versioning daemon
+    version_daemon = CodeVersioningDaemon(remote_name, remote_url, name_for_branch)
     # Version code
-    version_code(name_for_branch, remote_name, remote_url)
+    version_daemon.version_code()
 
     # Copy code
     root_dir = find_git_root()
@@ -34,141 +187,6 @@ def version_and_copy_code(
 
     # Change to the new directory
     os.chdir(newdir_path)
-
-
-def version_code(name_for_branch, remote_name, remote_url):
-    repo = Repo(find_git_root())
-
-    # Record current branch
-    current_branch = repo.active_branch.name
-
-    # Add remote if it does not exist
-    check_remote(repo, remote_name, remote_url)
-
-    # rebase on newest version of main
-    try:
-        rebase_on_new_main(name_for_branch, current_branch, repo)
-    except GitCommandError:
-        raise Exception("Failed to rebase on newest version of main. Aborting...")
-
-    try:
-        # Stage all changes
-        repo.git.add(u=True)
-
-        # Commit changes
-        repo.index.commit(message="commit before experiment", skip_hooks=True)
-
-    except GitCommandError:
-        repo.git.checkout(current_branch)
-        raise Exception("Failed to commit. Aborting...")
-
-    try:
-        # Push changes to the remote
-        repo.git.push(remote_name, name_for_branch)
-
-    except GitCommandError as e:
-        repo.git.checkout(current_branch)
-        # Handle exceptions, framed inside a GitCommandError
-        raise Exception(f"Failed to push changes, error occurred during {str(e)}")
-
-    finally:
-        # Return to original branch and reset changes - all previously unstaged changes will be as they were before
-        repo.git.checkout(current_branch)
-        repo.git.reset("--mixed", "HEAD~1")
-
-
-def rebase_on_new_main(name_for_branch, current_branch, repo):
-    # Check for changes in the current workspace
-    should_unstash = False
-    if repo.is_dirty():
-        should_unstash = True
-        # Changes exist, so let's stash them
-        try:
-            repo.git.stash()
-        except GitCommandError:
-            raise GitCommandError(
-                "Failed to stash changes. Not sure what to do. Aborting..."
-            )
-
-    # try to checkout main and pull
-    try:
-        # Switch to the 'main' branch
-        repo.git.checkout("main")
-    except GitCommandError:
-        if should_unstash:
-            repo.git.stash("pop")
-        raise GitCommandError(
-            "Failed to checkout main. Make sure you have a main branch."
-        )
-    try:
-        # Perform git pull
-        repo.git.pull()
-    except GitCommandError:
-        assert repo.active_branch.name == "main"
-        repo.git.reset("--hard", "HEAD")
-        repo.git.checkout(current_branch)
-        if should_unstash:
-            repo.git.stash("pop")
-        raise GitCommandError(
-            "Failed to pull from main. Make sure you have a main branch."
-        )
-    try:
-        repo.git.checkout(b=name_for_branch)
-
-    except GitCommandError:
-        assert repo.active_branch.name == "main"
-        repo.git.reset("--hard", "HEAD")
-        repo.git.checkout(current_branch)
-        if should_unstash:
-            repo.git.stash("pop")
-        raise GitCommandError("Failed to create new branch.")
-
-    try:
-        if should_unstash:
-            try:
-                # Use 'subprocess' module to call git stash pop
-                subprocess.run(["git", "stash", "pop"], check=True)
-            except subprocess.CalledProcessError as err:
-                print(
-                    "Error encountered while applying stashed changes.",
-                    err,
-                    "Trying to merge, favoring stash...",
-                )
-                try:
-                    subprocess.run(["git", "checkout", "--theirs", "."], check=True)
-                except subprocess.CalledProcessError as err:
-                    print(
-                        "Error encountered while resolving conflicts in favor of stash.",
-                        err,
-                    )
-                    raise GitCommandError(
-                        "Rebasing on new main failed, conflicts occurred. Could not resolve them automatically. "
-                    )
-
-    except GitCommandError:
-        # In case of conflict or any other git error, reset back to the initial state
-        repo.git.reset("--hard", "HEAD")
-        # Reset back to the initial recorded state
-        repo.git.checkout(current_branch)
-        if should_unstash:
-            # Unstash the changes
-            repo.git.stash("pop")
-        raise GitCommandError(
-            "An error occurred. There are conflicts between newest main and your local changes. Resolve them locally (e.g. by merging the newest main to your current branch) and try again. \nResetting back to initial state..."
-        )
-
-
-def check_remote(repo, remote_name, remote_url):
-    for remote in repo.remotes:
-        if remote.name == remote_name:
-            if remote.url == remote_url:
-                return
-            else:
-                raise Exception(
-                    f"Wrong url under remote repo {remote_name}: {repo.remotes[remote_name].url.strip()}, should be {remote_url}"
-                )
-    repo.create_remote(remote_name, url=remote_url)
-    return
 
 
 def find_git_root():
