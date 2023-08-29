7c360674fc9f5ab6a1e3d76b11eff1d0aada829d
Maciej Pióro <m@ciekpio.ro>
2023-08-29 16:59:00 +0200

    Remove files added with draft

--- lizrd/datasets/utils.py
+++
@@ -1,8 +0,0 @@
-import random
-
-
-def get_random_chunk(ids, length, rng=None):
-    if rng == None:
-        rng = random
-    beginning = rng.randint(0, len(ids) - length)
-    return ids[beginning : beginning + length]
