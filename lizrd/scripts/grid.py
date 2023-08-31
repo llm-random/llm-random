ce79fd677de116f6f90f2c3f9ebf8ebca635328f
Simontwice <szymon.antoniak1@gmail.com>
2023-09-01 01:05:05 +0200

    wip

--- lizrd/scripts/grid.py
+++ lizrd/scripts/grid.py
@@ -5,7 +5,6 @@ Remember to set RUNNER and PARAMS in the script or add an argument parser.
 """
 
 import datetime
-import json
 import os
 import subprocess
 import sys
@@ -27,87 +26,80 @@ from lizrd.scripts.grid_utils import (
 )
 from lizrd.support.code_versioning_support import copy_and_version_code
 
-RUNNER = "research.reinitialization.train.reinit_train"
-
-
-# ^ - grid over that
-# * - apply function
-PARAMS = {
-    "project_name": f"{os.getenv('USER')}/mp",
-    "name": "mp",
-    "ff_layer": "regular",
-    "batch_size": 128,
-    "cutoff": 128,
-    "^mixed_precision": [True, False],
-    "tags": ["test"],
-    "use_clearml": True,
-    "pruner_n_steps": 100,
-}
-
-TIME = "1-00:00:00"
-GRES = "gpu:titanv:1"
-DRY_RUN = False
-CODE_PATH = os.getcwd()
-INTERACTIVE_DEBUG = False
-RUNS_MULTIPLIER = 1
-PUSH_TO_GIT = False
-COPIED_CODE_PATH = ""
 if __name__ == "__main__":
     runner = get_machine_backend()
-    HF_DATASETS_CACHE = get_cache_path(runner)
-    SINGULARITY_IMAGE = get_sparsity_image(runner)
-    NODELIST = None
-    N_GPUS = 1
-    CPUS_PER_GPU = 8
-    CUDA_VISIBLE_DEVICES = None
     PROCESS_CALL_FUNCTION = lambda args, env: subprocess.run(
         [str(arg) for arg in args if arg is not None], env=env
     )
-
-    if len(sys.argv) > 1:
+    try:
         path = sys.argv[1]
-        if path.endswith(".json"):
-            grid_args = json.load(open(sys.argv[1]))
-        elif path.endswith(".yaml"):
-            grid_args = yaml.safe_load(open(sys.argv[1]))
-        else:
-            raise ValueError("config path must be .json or .yaml")
-
-        RUNNER = grid_args.get("runner", RUNNER)
-        PARAMS = grid_args.get("params", PARAMS)
-        TIME = grid_args.get("time", TIME)
-        GRES = grid_args.get("gres", GRES)
-        DRY_RUN = grid_args.get("dry_run", DRY_RUN)
-        SINGULARITY_IMAGE = grid_args.get("singularity_image", SINGULARITY_IMAGE)
-        HF_DATASETS_CACHE = grid_args.get("hf_datasets_cache", HF_DATASETS_CACHE)
-        RUNS_MULTIPLIER = grid_args.get("runs_multiplier", RUNS_MULTIPLIER)
-        INTERACTIVE_DEBUG = grid_args.get("interactive_debug", INTERACTIVE_DEBUG)
-        PUSH_TO_GIT = grid_args.get("push_to_git", PUSH_TO_GIT)
-        NODELIST = grid_args.get("nodelist", NODELIST)
-        N_GPUS = grid_args.get("n_gpus", N_GPUS)
-        CPUS_PER_GPU = grid_args.get("cpus_per_gpu", CPUS_PER_GPU)
-        CUDA_VISIBLE_DEVICES = grid_args.get("cuda_visible", CUDA_VISIBLE_DEVICES)
-
-    else:
+    except IndexError:
         raise ValueError("No config path specified. Aborting...")
 
-    if SINGULARITY_IMAGE is None and runner != MachineBackend.LOCAL:
-        raise ValueError("Singularity image is not specified (in JSON or env variable)")
+    if path.endswith(".yaml"):
+        configs = yaml.safe_load(open(sys.argv[1]))
+    else:
+        raise ValueError("config path must be a .yaml")
 
-    if NODELIST is not None:
-        NODELIST = "--nodelist=" + NODELIST
+    grid = []
+    total_no_experiments = 0
+    total_minutes = 0
 
-    grid = create_grid(PARAMS)
-    grid = multiply_grid(grid, RUNS_MULTIPLIER)
-    no_experiments = len(grid)
-    minutes_per_exp = timestr_to_minutes(TIME)
+    for grid_args in configs:
+        RUNNER = grid_args["runner"]
+        PARAMS = grid_args["params"]
+        TIME = grid_args.get("time", "1-00:00:00")
+        GRES = grid_args.get("gres", "gpu:titanv:1")
+        DRY_RUN = grid_args.get("dry_run", False)
+        SINGULARITY_IMAGE = grid_args.get(
+            "singularity_image", get_sparsity_image(runner)
+        )
+        HF_DATASETS_CACHE = grid_args.get("hf_datasets_cache", get_cache_path(runner))
+        RUNS_MULTIPLIER = grid_args.get("runs_multiplier", 1)
+        INTERACTIVE_DEBUG = grid_args.get("interactive_debug", False)
+        NODELIST = grid_args.get("nodelist", None)
+        N_GPUS = grid_args.get("n_gpus", 1)
+        CPUS_PER_GPU = grid_args.get("cpus_per_gpu", 8)
+        CUDA_VISIBLE_DEVICES = grid_args.get("cuda_visible", None)
+
+        if SINGULARITY_IMAGE is None and runner != MachineBackend.LOCAL:
+            raise ValueError(
+                "Singularity image is not specified (in JSON or env variable)"
+            )
+
+        if NODELIST is not None:
+            NODELIST = "--nodelist=" + NODELIST
+
+        for name, param in zip(
+            [
+                "gres",
+                "time",
+                "n_gpus",
+                "runner",
+                "cpus_per_gpu",
+                "nodelist",
+                "cuda_visible",
+            ],
+            [GRES, TIME, N_GPUS, RUNNER, CPUS_PER_GPU, NODELIST, CUDA_VISIBLE_DEVICES],
+        ):
+            PARAMS["temp_args"][name] = param
+
+        partial_grid = create_grid(PARAMS)
+        partial_grid = multiply_grid(partial_grid, RUNS_MULTIPLIER)
+        grid.extend(partial_grid)
+
+        no_experiments = len(partial_grid)
+        total_no_experiments += no_experiments
+
+        minutes_per_exp = timestr_to_minutes(TIME)
+        total_minutes_from_this_grid = no_experiments * minutes_per_exp
+        total_minutes += total_minutes_from_this_grid
 
     if len(grid) > 1 and runner == MachineBackend.LOCAL and not DRY_RUN:
         raise ValueError(
             f"Running more than one experiment locally is not supported (you are trying to run {len(grid)} experiments). Aborting..."
         )
 
-    total_minutes = no_experiments * minutes_per_exp
     if not runner == MachineBackend.LOCAL:
         if not INTERACTIVE_DEBUG:
             user_input = input(
@@ -121,6 +113,12 @@ if __name__ == "__main__":
             exit(1)
 
     slurm_command = "srun" if INTERACTIVE_DEBUG else "sbatch"
+    assert all(
+        [
+            grid_args.get("INTERACTIVE_DEBUG") == INTERACTIVE_DEBUG
+            for grid_args in configs
+        ]
+    ), "`interactive_debug` must be the same for all configs"
 
     if not (INTERACTIVE_DEBUG or runner == MachineBackend.LOCAL):
         exp_name = next(iter(grid))["name"]
@@ -128,7 +126,7 @@ if __name__ == "__main__":
             f"{exp_name}_{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
         )
         COPIED_CODE_PATH = copy_and_version_code(
-            name_for_branch, name_for_branch, PUSH_TO_GIT
+            name_for_branch, name_for_branch, False
         )
     else:
         print(
@@ -137,11 +135,13 @@ if __name__ == "__main__":
 
     for i, param_set in enumerate(grid):
         name = param_set["name"]
-        param_set["n_gpus"] = N_GPUS
+        param_set["n_gpus"] = param_set["temp_args"]["n_gpus"]
         env = None
 
         runner_params = []
         for k_packed, v_packed in param_set.items():
+            if k_packed == "temp_args":
+                continue
             for k, v in zip(*unpack_params(k_packed, v_packed)):
                 if isinstance(v, bool):
                     if v:
@@ -161,81 +161,85 @@ if __name__ == "__main__":
                 slurm_command,
                 "--partition=common",
                 "--qos=16gpu7d",
-                f"--gres={GRES}",
+                f"--gres={param_set['temp_args']['gres']}",
                 f"--job-name={name}",
-                f"--time={TIME}",
+                f"--time={param_set['temp_args']['time']}",
                 get_grid_entrypoint(runner),
                 "python3",
                 "-m",
-                RUNNER,
+                param_set["temp_args"]["runner"],
                 *runner_params,
             ]
         elif runner == MachineBackend.ATHENA:
             subprocess_args = [
                 slurm_command,
-                f"--gres=gpu:{N_GPUS}",
+                f"--gres=gpu:{param_set['temp_args']['n_gpus']}",
                 "--partition=plgrid-gpu-a100",
-                f"--cpus-per-gpu={CPUS_PER_GPU}",
+                f"--cpus-per-gpu={param_set['temp_args']['cpus_per_gpu']}",
                 "--account=plgplggllmeffi-gpu-a100",
                 f"--job-name={name}",
-                f"--time={TIME}",
+                f"--time={param_set['temp_args']['time']}",
                 get_grid_entrypoint(runner),
                 "singularity",
                 "run",
                 "--bind=/net:/net",
                 f"--env",
                 f"HF_DATASETS_CACHE={HF_DATASETS_CACHE}",
-                f"-B={CODE_PATH}:/sparsity,{HF_DATASETS_CACHE}:{HF_DATASETS_CACHE}",
+                f"-B={COPIED_CODE_PATH}:/sparsity,{HF_DATASETS_CACHE}:{HF_DATASETS_CACHE}",
                 "--nv",
                 SINGULARITY_IMAGE,
                 "python3",
                 "-m",
-                RUNNER,
+                param_set["temp_args"]["runner"],
                 *runner_params,
             ]
         elif runner == MachineBackend.IDEAS:
             subprocess_args = [
                 slurm_command,
-                f"--gres=gpu:{N_GPUS}",
-                f"--cpus-per-gpu={CPUS_PER_GPU}",
+                f"--gres=gpu:{param_set['temp_args']['n_gpus']}",
+                f"--cpus-per-gpu={param_set['temp_args']['cpus_per_gpu']}",
                 f"--job-name={name}",
-                f"--time={TIME}",
+                f"--time={param_set['temp_args']['time']}",
                 "--mem=32G",
-                NODELIST,
+                param_set["temp_args"]["nodelist"],
                 get_grid_entrypoint(runner),
                 "singularity",
                 "run",
                 f"--env",
                 f"HF_DATASETS_CACHE={HF_DATASETS_CACHE}",
-                f"-B={CODE_PATH}:/sparsity,{HF_DATASETS_CACHE}:{HF_DATASETS_CACHE}",
+                f"-B={COPIED_CODE_PATH}:/sparsity,{HF_DATASETS_CACHE}:{HF_DATASETS_CACHE}",
                 "--nv",
                 SINGULARITY_IMAGE,
                 "python3",
                 "-m",
-                RUNNER,
+                param_set["temp_args"]["runner"],
                 *runner_params,
             ]
         elif runner == MachineBackend.ENTROPY_GPU:
-            if CUDA_VISIBLE_DEVICES is not None:
+            if param_set["temp_args"]["cuda_visible"] is not None:
                 env = os.environ.copy()
-                env.update({"CUDA_VISIBLE_DEVICES": CUDA_VISIBLE_DEVICES})
+                env.update(
+                    {"CUDA_VISIBLE_DEVICES": param_set["temp_args"]["cuda_visible"]}
+                )
             subprocess_args = [
                 "singularity",
                 "run",
                 f"--env",
                 f"HF_DATASETS_CACHE={HF_DATASETS_CACHE}",
-                f"-B={CODE_PATH}:/sparsity,{HF_DATASETS_CACHE}:{HF_DATASETS_CACHE}",
+                f"-B={COPIED_CODE_PATH}:/sparsity,{HF_DATASETS_CACHE}:{HF_DATASETS_CACHE}",
                 "--nv",
                 SINGULARITY_IMAGE,
                 "python3",
                 "-m",
-                RUNNER,
+                param_set["temp_args"]["runner"],
                 *runner_params,
             ]
         elif runner == MachineBackend.LOCAL:
             # We run the experiment directly, not through a grid entrypoint script
             # because we want to be able to debug it
-            runner_main_function = get_train_main_function(RUNNER)
+            runner_main_function = get_train_main_function(
+                param_set["temp_args"]["runner"]
+            )
             runner_main_function(None, runner_params=runner_params)
             exit(0)
         else:
