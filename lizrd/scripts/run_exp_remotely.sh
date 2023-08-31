640883523dc7fe7542177dc199b74e888e960a79
Simontwice <szymon.antoniak1@gmail.com>
2023-08-31 10:52:27 +0200

    wip

--- lizrd/scripts/run_exp_remotely.sh
+++ lizrd/scripts/run_exp_remotely.sh
@@ -3,7 +3,7 @@
 # 1. needs to be called from sparsity folder
 # 2. needs to be called with the host as the first argument (as configured in ~/.ssh/config, e.g. gpu_entropy)
 # 3. needs to be called with the config file as the second argument (e.g. "my_config.yaml")
-
+# EXAMPLE USAGE: bash lizrd/scripts/run_exp_remotely.sh atena quick.json
 set -e
 
 source venv/bin/activate
