379d8cdb6960f06589fef02deaa1c216877102e5
ludziej <ludziej@mimuw.edu.pl>
2023-09-01 02:23:59 +0200

    Integration tests (#192)
    * commit to run tests
    
    * fixed tests path
    
    * integration tests running on test neptune project

---
+++ research/conditional/tests/test_integrated.py
@@ -0,0 +1,21 @@
+from lizrd.support.test_utils import GeneralTestCase
+from pathlib import Path
+import subprocess
+
+
+class TestIntegrated(GeneralTestCase):
+    def test_dummy_train(self):
+        """
+        Test if the training loop runs without crashing, using dummy data
+        from configs defined in research/conditional/train/configs/test/*.yaml
+        """
+
+        configs = (Path(__file__).parent.resolve() / "../train/configs/test/").glob(
+            "**/*.yaml"
+        )
+        for path in configs:
+            print(f"Running training loop with config from {str(path)}")
+            exit_code = subprocess.call(
+                ["python3", "-m", "lizrd.scripts.grid", str(path)]
+            )
+            assert exit_code == 0
