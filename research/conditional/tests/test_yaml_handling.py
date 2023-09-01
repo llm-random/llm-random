747487b98efa8f9917eb38e755218676601fc0f3
Simontwice <szymon.antoniak1@gmail.com>
2023-09-01 12:41:44 +0200

    wip

---
+++ research/conditional/tests/test_yaml_handling.py
@@ -0,0 +1,17 @@
+from lizrd.support.misc import load_with_inheritance
+from lizrd.support.test_utils import GeneralTestCase
+
+
+class TestYaml(GeneralTestCase):
+    def test_yaml(self):
+        """
+        Test if the yaml config with multiple configs and inheritance is loaded correctly
+        """
+        path_to_yaml = "research/conditional/train/configs/test/test.yaml"
+        configs = load_with_inheritance(path_to_yaml)
+        assert len(configs) == 2 + 20
+        configs = list[configs]
+        for config in configs:
+            assert config["params"]["dhead"] == 100
+        assert configs[0]["runs_multiplier"] == 20
+        assert configs[1]["runs_multiplier"] == 1
