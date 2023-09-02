b3f01024a5dc4c82819a465f74f839c67f6baca1
Simontwice <szymon.antoniak1@gmail.com>
2023-09-02 12:11:41 +0200

    wip

--- lizrd/scripts/tests/test_yaml_handling.py
+++ lizrd/scripts/tests/test_yaml_handling.py
@@ -7,17 +7,18 @@ class TestYaml(GeneralTestCase):
         """
         Test if the yaml config with multiple configs and inheritance is loaded correctly
         """
-        path_to_yaml = "research/conditional/tests/configs/test_yaml_loading.yaml"
+        path_to_yaml = "lizrd/scripts/tests/configs/test_yaml_loading.yaml"
         configs, paths = load_with_inheritance(path_to_yaml)
         configs = list(configs)
         assert paths == {
-            "research/conditional/tests/configs/test_yaml_loading.yaml",
-            "research/conditional/tests/configs/test_yaml_loading2.yaml",
+            "lizrd/scripts/tests/configs/test_yaml_loading.yaml",
+            "lizrd/scripts/tests/configs/test_yaml_loading2.yaml",
+            "lizrd/scripts/tests/configs/test_yaml_loading3.yaml",
         }
         for config in configs:
             print(config)
         assert len(configs) == 2
         assert configs[0]["params"]["dhead"] == 21
-        assert configs[1]["params"]["dhead"] == 210
+        assert configs[1]["params"]["dhead"] == 100
         assert configs[0]["runs_multiplier"] == 20
         assert configs[1]["runs_multiplier"] == 1
