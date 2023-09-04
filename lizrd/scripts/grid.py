5b202971c0cecd6a11b4cd184699fe836b55df03
Simontwice <szymon.antoniak1@gmail.com>
2023-09-04 16:41:18 +0200

    ww

--- lizrd/scripts/grid.py
+++ lizrd/scripts/grid.py
@@ -57,25 +57,25 @@ if __name__ == "__main__":
 
     # list of pairs: a dictionary of training_args and a dictionary of setup_args
     grid = []
-    total_no_experiments = 0
+    total_n_experiments = 0
     total_minutes = 0
 
-    for i, grid_args in enumerate(configs):
+    for i, configs in enumerate(configs):
         print(f"\nProcessing config {i}...")
-        pprint.pprint(grid_args)
-        single_exp_training_args_grid = create_grid(grid_args["params"])
+        pprint.pprint(configs)
+        single_exp_training_args_grid = create_grid(configs["params"])
 
-        setup_args = get_setup_args_with_defaults(grid_args, CLUSTER_NAME)
+        setup_args = get_setup_args_with_defaults(configs, CLUSTER_NAME)
         single_exp_training_args_grid = multiply_grid(
             single_exp_training_args_grid, setup_args["runs_multiplier"]
         )
-        no_experiments = len(single_exp_training_args_grid)
+        n_experiments = len(single_exp_training_args_grid)
 
-        grid += list(zip(single_exp_training_args_grid, [setup_args] * no_experiments))
+        grid += list(zip(single_exp_training_args_grid, [setup_args] * n_experiments))
 
-        total_no_experiments += no_experiments
+        total_n_experiments += n_experiments
         minutes_per_exp = timestr_to_minutes(setup_args["time"])
-        total_minutes_from_this_grid = no_experiments * minutes_per_exp
+        total_minutes_from_this_grid = n_experiments * minutes_per_exp
         total_minutes += total_minutes_from_this_grid
 
     if CLUSTER_NAME == MachineBackend.LOCAL and len(grid) > 1:
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     if not CLUSTER_NAME == MachineBackend.LOCAL:
         if not interactive_debug_session:
             user_input = input(
-                f"Will run {total_no_experiments} experiments, using up {total_minutes} minutes, i.e. around {round(total_minutes / 60)} hours\n"
+                f"Will run {total_n_experiments} experiments, using up {total_minutes} minutes, i.e. around {round(total_minutes / 60)} hours\n"
                 f"Continue? [Y/n]"
                 f"Will run {total_n_experiments} experiments, using up {total_minutes} minutes, i.e. around {round(total_minutes / 60)} hours\n"
                 f"Continue? [Y/n]"
