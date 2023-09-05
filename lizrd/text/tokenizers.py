0da0d762b6c014b9dbeca8ec6aad67714f9540c1
Maciej Pióro <m@ciekpio.ro>
2023-09-05 11:33:39 +0200

    Some changes to tokenizers

--- lizrd/text/tokenizers.py
+++ lizrd/text/tokenizers.py
@@ -14,16 +14,22 @@ class AbstractTokenizer(ABC):
         raise NotImplementedError()
 
 
+def disable_tokenizer_warnings(hf_tokenizer):
+    # set model max length to high number to disable warnings
+    # we handle sequence length ourselves
+    hf_tokenizer.model_max_length = 100_000
+
+
 class BertTokenizer(AbstractTokenizer):
     vocab_size = 30522
 
     def __init__(self):
         self.tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+        disable_tokenizer_warnings(self.tokenizer)
         self.mask_id = self.tokenizer.convert_tokens_to_ids("[MASK]")
-        # set model max length to high number to disable warnings
-        # we handle sequence length ourselves
-        self.tokenizer.model_max_length = 100_000
+        assert isinstance(self.mask_id, int)
         self.sequence_separator_id = self.tokenizer.convert_tokens_to_ids("[SEP]")
+        assert isinstance(self.sequence_separator_id, int)
 
     def text_to_ids(self, text: str) -> List[int]:
         # TODO: encode or tokenize + convert_tokens_to_ids?
@@ -35,10 +41,9 @@ class GPTTokenizer(AbstractTokenizer):
 
     def __init__(self):
         self.tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
-        # set model max length to high number to disable warnings
-        # we handle sequence length ourselves
-        self.tokenizer.model_max_length = 100_000
+        disable_tokenizer_warnings(self.tokenizer)
         self.eot_id = self.tokenizer.convert_tokens_to_ids("<|endoftext|>")
+        assert isinstance(self.eot_id, int)
 
     def text_to_ids(self, text: str) -> List[int]:
         # TODO: encode or tokenize + convert_tokens_to_ids?
