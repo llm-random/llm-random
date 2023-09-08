1ebe5c0c6392db13c969e118abc6f78a62735aed
Simontwice <szymon.antoniak1@gmail.com>
2023-09-08 20:03:54 +0200

    ww

--- research/conditional/utils/model_utils.py
+++ research/conditional/utils/model_utils.py
@@ -39,6 +39,9 @@ from research.conditional.moe_layers.cont_moe_designs.separate_merge_emit_weight
 from research.conditional.moe_layers.cont_moe_designs.separate_weighted_parameter_matrices import (
     ContinuousMoESeparateWeightedParameters,
 )
+from research.conditional.moe_layers.cont_moe_designs.softmax_over_experts_for_emit import (
+    ContinuousMoESoftmaxOverExperts,
+)
 from research.conditional.moe_layers.continuous_moe import (
     ContinuousMoE,
 )
@@ -437,6 +440,18 @@ def get_ff_layer(args):
         )
     elif args.ff_mode == "cont_moe_separate_weighted_parameters":
         return_fn = lambda: ContinuousMoESeparateWeightedParameters(
+            dm=args.dmodel,
+            dff=args.dff,
+            n_experts=args.n_experts,
+            group_size=args.group_size,
+            sparsity_dim=args.sparsity_dim,
+            temperature=args.temperature,
+            expert_size=args.expert_size,
+            use_opt_einsum=args.use_opt_einsum,
+            flop_matched=args.flop_matched,
+        )
+    elif args.ff_mode == "cont_moe_softmoe_emit":
+        return_fn = lambda: ContinuousMoESoftmaxOverExperts(
             dm=args.dmodel,
             dff=args.dff,
             n_experts=args.n_experts,
