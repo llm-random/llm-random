9254c0cb3c8ef424ad40f701f9a2ea70e5a6270b
Simontwice <szymon.antoniak1@gmail.com>
2023-09-21 15:11:57 +0200

    ww

--- research/conditional/moe_layers/cont_moe_designs/learnable_temperature.py
+++ research/conditional/moe_layers/cont_moe_designs/learnable_temperature.py
@@ -1,13 +1,10 @@
 import dataclasses
 
-import numpy as np
 import torch
-from plotly import express as px
 
 from lizrd.core import misc, nn
-from lizrd.support.logging import make_histogram
 from research.conditional.moe_layers.continuous_moe import ContinuousMoeBaseClass
-from research.conditional.utils.misc_tools import stable_softmax_temperature, entropy
+from research.conditional.utils.misc_tools import stable_softmax_temperature
 
 
 @dataclasses.dataclass(eq=False, repr=False)
@@ -63,42 +60,11 @@ class ContinuousMoEAdaTemp(ContinuousMoeBaseClass):
         )
 
     def log_heavy(self):
-        log = {}
-        if self.group_size == 1:
-            return log
-        merge_weights = torch.flatten(
-            self.logging_cache["merge_weights"], start_dim=0, end_dim=-2
-        )
-        merge_logits = torch.flatten(
-            self.logging_cache["merge_logits"], start_dim=0, end_dim=-2
-        )
-        sample_weight_distros = merge_weights[:5]
-        sample_logits_distros = merge_logits[:5]
-
-        for i, sample in enumerate(sample_weight_distros):
-            sample = sample.sort(descending=True).values
-            sample = sample.tolist()
-            fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
-            log[f"merge_weights/sample_{i}"] = fig
-
-        for i, sample in enumerate(sample_logits_distros):
-            sample = sample.sort(descending=True).values
-            sample = sample.tolist()
-            fig = px.bar(x=range(len(sample)), y=sample, title=f"sample {i}")
-            log[f"merge_logits/sample_{i}"] = fig
-
-        ent = entropy(merge_weights)
-        max_entropy = np.log(self.n_experts)
-        normalised_ent = ent / max_entropy
-        log["merge_weights/normalised_entropy"] = make_histogram(
-            normalised_ent, title="merge logits entropy (normalised to [0,1])"
-        )
-
+        log = self.super().log_heavy()
         log[
             "merge_weights/merge_temperature"
         ] = self.temperature_merge.data.flatten().tolist()
         log[
             "merge_weights/emit_temperature"
         ] = self.temperature_emit.data.flatten().tolist()
-
         return log
