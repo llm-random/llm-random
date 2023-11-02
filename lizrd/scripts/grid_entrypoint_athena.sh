71763d9ef39e5b78696c5880d3dcca7c70c1bc57
Maciej Pióro <m@ciekpio.ro>
2023-11-02 16:46:42 +0100

    WIP

--- lizrd/scripts/grid_entrypoint_athena.sh
+++ lizrd/scripts/grid_entrypoint_athena.sh
@@ -4,8 +4,4 @@ module CUDA/11.7.0
 echo "Will run the following command:"
 echo "$@"
 echo "==============================="
-env
-echo "==============================="
-env | grep NEPTUNE
-exit
 $@
