69911f929c0d8fb15e4d79724df2e7e8d0d2f171
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2023-11-07 18:11:44 +0100

    wip

--- research/conditional/utils/argparse.py
+++ research/conditional/utils/argparse.py
@@ -11,9 +11,7 @@ def introduce_parser_arguments(
     parser.add_argument("--ff_mode", type=str, default="vanilla")
     parser.add_argument("--n_blocks", type=int, required=True)
     parser.add_argument("--dmodel", type=int, required=True)
-    parser.add_argument(
-        "--dff", type=int, required=False
-    )  # not used by granularity
+    parser.add_argument("--dff", type=int, required=False)  # not used by granularity
     parser.add_argument("--n_att_heads", type=int, required=True)
     parser.add_argument("--dhead", type=int, default=None)
 
