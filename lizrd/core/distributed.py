a6aa745f077ade07d398d6623e623ec97cfec8ac
Kuba Krajewski <gim.jakubk@gmail.com>
2023-11-09 10:15:45 +0100

    Refactor distributed

--- lizrd/core/distributed.py
+++ lizrd/core/distributed.py
@@ -9,31 +9,6 @@ import torch
 from lizrd.core.misc import Noop
 
 
-def wrap_module_in_fsdp(
-    module: nn.Module,
-    rank: Optional[int],
-    param_precision: torch.dtype,
-    cast_inputs: bool = False,
-    offload_params: bool = False,
-    print_model: bool = False,
-):
-    wrapped = FSDP(
-        module,
-        device_id=rank,
-        mixed_precision=MixedPrecision(
-            param_dtype=param_precision,
-            reduce_dtype=torch.float32,
-            cast_forward_inputs=cast_inputs,
-        ),
-        cpu_offload=CPUOffload(offload_params=offload_params),
-    )
-    if print_model:
-        print("------- MODEL AFTER WRAPPING IN FSDP -------")
-        print(wrapped)
-        print("--------------------------------------------")
-    return wrapped
-
-
 def wrap_in_fsdp(
     enabled: bool,
     module: nn.Module,
@@ -46,31 +21,32 @@ def wrap_in_fsdp(
 ):
     if not enabled:
         return module
-    elif cast_outputs_to is not None:
-        cast_module = wrap_module_in_fsdp(
-            module=Noop(),
-            rank=rank,
-            param_precision=cast_outputs_to,
-            cast_inputs=True,
+
+    def _create_single_fsdp_module(module_to_wrap, precision):
+        return FSDP(
+            module_to_wrap,
+            device_id=rank,
+            mixed_precision=MixedPrecision(
+                param_dtype=precision,
+                reduce_dtype=torch.float32,
+                cast_forward_inputs=cast_inputs,
+            ),
+            cpu_offload=CPUOffload(offload_params=offload_params),
         )
-        main_module = wrap_module_in_fsdp(
-            module=module,
-            rank=rank,
-            param_precision=param_precision,
-            cast_inputs=cast_inputs,
-            offload_params=offload_params,
-            print_model=print_model,
-        )
-        return CastWrapper(module=main_module, cast_module=cast_module)
+
+    if cast_outputs_to is not None:
+        main_module = _create_single_fsdp_module(module, param_precision)
+        cast_module = _create_single_fsdp_module(Noop(), cast_outputs_to)
+        wrapped = CastWrapper(module=main_module, cast_module=cast_module)
     else:
-        return wrap_module_in_fsdp(
-            module=module,
-            rank=rank,
-            param_precision=param_precision,
-            cast_inputs=cast_inputs,
-            offload_params=offload_params,
-            print_model=print_model,
-        )
+        wrapped = _create_single_fsdp_module(module, param_precision)
+
+    if print_model:
+        print("------- MODEL AFTER WRAPPING IN FSDP -------")
+        print(wrapped)
+        print("--------------------------------------------")
+
+    return wrapped
 
 
 class CastWrapper(nn.Module):
