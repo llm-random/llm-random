f4a9e14a326fbe8ffa2cc5ce69380b78ef2f313d
Simontwice <szymon.antoniak1@gmail.com>
2023-11-10 15:56:39 +0100

    w

--- research/conditional/moe_layers/continuous_moe.py
+++ research/conditional/moe_layers/continuous_moe.py
@@ -226,7 +226,7 @@ class ContinuousMoE(ContinuousMoeBaseClass):
 def argmax_one_hot(x: torch.Tensor, dim: int):
     max_values, _ = x.max(dim=dim, keepdim=True)
     return torch.where(
-        x == max_values, 1.0, 1.0
+        x == max_values, 1.0, 0.0
     )  # potentially make it the value itself? torch.where(x == max_values, x, 0.0)
 
 
