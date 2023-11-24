3e8bd884ad6af487400090f6eb4bdef0dfd8a071
Maciej Pióro <m@ciekpio.ro>
2023-11-24 11:46:18 +0100

    Add a comment

--- research/blanks/test_tokenizers.py
+++ research/blanks/test_tokenizers.py
@@ -4,6 +4,7 @@ from research.blanks.tokenizers import MAX_BLANKS, BlankTokenizer
 
 class TestUtils(GeneralTestCase):
     def test_blank_tokens_ids(self):
+        """Tests that the blank tokens ids are correct. This test is potentially heavy - if tests become unbearably slow, consider marking it as with @heavy_test."""
         tokenizer = BlankTokenizer()
         static_ids = BlankTokenizer.BLANK_IDS
         actual_blank_ids = tokenizer.blank_ids
