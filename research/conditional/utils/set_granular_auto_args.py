8fa8892415bdb9eb5697f731a0d7b0f4d89cbd5c
Kuba Krajewski <gim.jakubk@gmail.com>
2023-12-14 16:01:05 +0100

    fix bug in params definition

--- research/conditional/utils/set_granular_auto_args.py
+++ research/conditional/utils/set_granular_auto_args.py
@@ -2,7 +2,9 @@ def set_granular_auto_args(args):
     # check everything not required is set
     assert args.batch_size_per_gpu is not None
 
-    args.gradient_accumulation_steps = 8 / args.n_gpus
+    args.gradient_accumulation_steps = int(8 / args.n_gpus)
     args.final_lr_fraction = 0.1
     args.final_lr_step = args.n_steps
-    args.lr_warmup_steps = round(args.n_steps * 0.01)
+    args.lr_warmup_steps = int(round(args.n_steps * 0.01))
+
+    return args
