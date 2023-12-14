b469c8b5a19657ee07f65260b3e32fe98fe9212f
Maciej Pióro <m@ciekpio.ro>
2023-12-14 17:02:44 +0100

    Fix building image

--- install_requirements.sh
+++ install_requirements.sh
@@ -8,5 +8,5 @@ python3 -m pip install -U multiprocess
 if ! command -v nvcc &> /dev/null; then
     echo "CUDA Toolkit is not installed. Skipping installation of mamba dependencies."
 else
-    python3 -m pip install -r mamba_requirements.txt
+    python3 -m pip install --no-build-isolation -r mamba_requirements.txt
 fi
