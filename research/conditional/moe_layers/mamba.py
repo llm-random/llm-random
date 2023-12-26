009f6f47e6b49baec5a2dd377b51107138e04a1d
Krystian-Krol <krystian.kytek@gmail.com>
2023-12-26 20:59:31 +0100

    Dirty monkey-patching

---
+++ research/conditional/moe_layers/mamba.py
@@ -0,0 +1,40 @@
+import torch
+from einops import rearrange
+
+from research.conditional.moe_layers.expert_choice import ExpertChoiceFF
+from research.conditional.utils.layer_manager import LoggingLayer
+
+
+class MambaInProj(LoggingLayer):
+    def __init__(self, batch_size, conv_proj, gate_proj):
+        super().__init__()
+        self.conv_proj = conv_proj
+        self.gate_proj = gate_proj
+        self.bias = None
+        self.batch_size = batch_size
+
+    @property
+    def weight(self):
+        return self
+
+    def forward(self, x):
+        x = rearrange(x, "d (b l) -> b l d", b=self.batch_size)
+        return torch.cat(
+            (
+                rearrange(self.conv_proj(x), "b l d -> d (b l)"),
+                rearrange(self.gate_proj(x), "b l d -> d (b l)"),
+            )
+        ).type(torch.bfloat16)
+
+    def __matmul__(self, other):
+        return self.forward(other)
+
+
+class MambaConvMoE(ExpertChoiceFF):
+    def __init__(self, batch_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.batch_size = batch_size
+
+    def forward(self, x):
+        x = rearrange(x, "d (b l) -> b l d", b=self.batch_size)
+        return rearrange(super().forward(x), "b l d -> d (b l)")
