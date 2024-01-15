3d546d8f01be323c2aa3ecd3e5700b7ebc1a1d89
Krystian-Krol <krystian.kytek@gmail.com>
2024-01-15 12:36:43 +0100

    Avg 8 steps in past

--- research/conditional/moe_layers/titram.py
+++ research/conditional/moe_layers/titram.py
@@ -9,6 +9,15 @@ from lizrd.support.logging import make_histogram
 from research.conditional.utils.layer_manager import LoggingLayer
 
 
+# class DummyMamba:
+#     def __init__(self, d_model):
+#         self.dmodel = d_model
+#
+#     def forward(self, x: torch.Tensor):
+#         batch_size, seq_len = x.shape[0], x.shape[1]
+#         return torch.rand(batch_size, seq_len, self.dmodel)
+
+
 class TiTraMamba(LoggingLayer):
     def __init__(
         self,
@@ -42,21 +51,24 @@ class TiTraMamba(LoggingLayer):
         x: (B, L, D)
         Returns: same shape as x
         """
-        seq_len = x.shape[1]
-        lookback_limit = torch.arange(seq_len, device=x.device)
+        batch_size, seq_len = x.shape[0], x.shape[1]
+        lookback_limit = torch.arange(seq_len, device=x.device, dtype=torch.int64)
 
         mamba_output = self.mamba.forward(x)
-        lookback_weight = torch.matmul(mamba_output, self.weight).view(-1, seq_len, 1)
-        lookback_regression = torch.add(torch.matmul(mamba_output, self.regression), 1)
-        lookback_regression = torch.round(self.non_neg(lookback_regression)).type(
-            torch.int64
+        lookback_weight = torch.matmul(mamba_output, self.weight).view(
+            batch_size, seq_len, 1
         )
+        lookback_regression = torch.add(torch.matmul(mamba_output, self.regression), 8)
+        lookback_regression = torch.round(lookback_regression).type(torch.int64)
         self.update_cache_for_logging("regression", lookback_regression)
         self.update_cache_for_logging("weight", lookback_weight)
+        lookback_regression = self.non_neg(
+            torch.sub(lookback_limit, lookback_regression)
+        )
         lookback_regression = torch.cumsum(lookback_regression, dim=1)
         lookback_regression = torch.clamp(lookback_regression, max=lookback_limit)
-        lookback_regression = lookback_regression.view(-1, seq_len, 1).expand(
-            -1, -1, self.dmodel
+        lookback_regression = lookback_regression.view(batch_size, seq_len, 1).expand(
+            batch_size, seq_len, self.dmodel
         )
         lookback = torch.gather(mamba_output, 1, lookback_regression)
         return mamba_output + lookback_weight * lookback
