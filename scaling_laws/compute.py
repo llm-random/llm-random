bc4764c056084612bb4c9f103b0c0ef1f5e95a04
Jan Ludziejewski <ludziej@mimuw.edu.pl>
2024-01-18 17:14:15 +0100

    new params

--- scaling_laws/compute.py
+++ scaling_laws/compute.py
@@ -10,7 +10,7 @@ from scaling_laws.utils import neptune_connect, download_batch_sizes_from_neptun
 from scaling_laws.calculate_params import calculate_active_params
 
 
-def plot_loss_vs_predicted_loss(scaling_law, group_by="granularity"):
+def plot_loss_vs_predicted_loss(scaling_law, no_title=False, group_by="granularity"):
     groups = get_groups_by_dim(group_by, scaling_law)
     colors = cm.rainbow(np.linspace(0, 1, len(groups)))
     A = np.array([scaling_law.expected_logloss(**r.dict()).detach().numpy() for r in scaling_law.runs])
@@ -26,7 +26,8 @@ def plot_loss_vs_predicted_loss(scaling_law, group_by="granularity"):
     plt.ylabel("ln(perplexity)")
     legend = plt.legend()
     rmse = scaling_law()[1]
-    plt.title(f"Loss vs predicted loss for {scaling_law.name} (RMSE={rmse:.3f})")
+    if not no_title:
+        plt.title(f"Loss vs predicted loss for {scaling_law.name} (RMSE={rmse:.3f})")
     plt.tight_layout()
     filename = f"scaling_laws/plots/{scaling_law.name}/error_{group_by}.png"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
@@ -34,13 +35,13 @@ def plot_loss_vs_predicted_loss(scaling_law, group_by="granularity"):
     plt.show()
 
 
-def plot_params(scaling_laws, plot_dim, show_model_sizes, extrapolate_factor=2.0, plot_points=100, **_):
+def plot_params(scaling_laws, plot_dim, show_model_sizes, extrapolate_factor=2.0, plot_points=100, no_title=False, **_):
     axis_dim = plot_dim[0]
     if axis_dim == "predicted_loss":
         for scaling_law in scaling_laws:
             if any([d in scaling_law.fixed_params for d in plot_dim[1:]]):
                 continue
-            plot_loss_vs_predicted_loss(scaling_law, group_by=plot_dim[1:])
+            plot_loss_vs_predicted_loss(scaling_law, group_by=plot_dim[1:], no_title=no_title)
         return
     if axis_dim == "flops_save":
         flops_save = True
@@ -66,7 +67,7 @@ def plot_params(scaling_laws, plot_dim, show_model_sizes, extrapolate_factor=2.0
         for (group, indices), color in zip(groups, colors):
             group_dict = dict(zip(group_dims, group))
             names.append(f"{scaling_law.name} {' '.join(f'{name}={int(val)}' for name, val in group_dict.items())}")
-            plt.scatter(A[indices], B[indices], color=color, s=5)
+            #plt.scatter(A[indices], B[indices], color=color, s=5)
             results = [scaling_law.resolve_params(**group_dict, **{axis_dim: np.power(10, a)}) for a in A_values]
             b_preds, b_opt_params = zip(*results)
             B_predictions.append(b_preds)
@@ -91,7 +92,7 @@ def plot_params(scaling_laws, plot_dim, show_model_sizes, extrapolate_factor=2.0
         top = max(top, min(B.max() + 0.2*(B.max() - B.min()), B_predictions.max()))
         bottom = min(bottom, min(B_predictions.min(), B.min()))
 
-        if full_flops:
+        if full_flops and scaling_law.name != 'dense':
             for k, (size, perplexity, params) in model_sizes.items():
                 if not np.isfinite(perplexity) and perplexity > 0:
                     continue
@@ -100,7 +101,8 @@ def plot_params(scaling_laws, plot_dim, show_model_sizes, extrapolate_factor=2.0
                 active_params = calculate_active_params(**params)
                 print(f"{k} steps={params['n_steps']:.2E} flops={params['flops']:.2E} perplexity={perplexity:.2E} active_params={active_params:.2E}")
 
-    plt.title('\n'.join([str(s) for s in scaling_laws]), wrap=True, fontsize=5)
+    if not no_title:
+        plt.title('\n'.join([str(s) for s in scaling_laws]), wrap=True, fontsize=5)
     plt.rc('font', size=7)
     plt.ylim(top=top, bottom=bottom if np.isfinite(bottom) else 0)
     plt.rc('legend', fontsize=5)
@@ -119,8 +121,8 @@ def plot_params(scaling_laws, plot_dim, show_model_sizes, extrapolate_factor=2.0
 def one_scaling(project, tags, fixed, tags_negative=(), **params):
     runs = download_batch_sizes_from_neptune(project, tags, tags_negative, fixed)
     scaling_law = ScalingLaw(runs=runs, fixed=fixed, **params)
-    rmse = scaling_law.optimize()
-    print(f"Final {scaling_law.name} scaling law approximation RMSE: {rmse}")
+    _ = scaling_law.optimize()
+    print(f"Final {scaling_law.name} scaling law approximation RMSE: {scaling_law()[1]}")
     scaling_law.present_values_as_chinchila()
     return scaling_law
 
