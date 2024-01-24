4dc2a819f03c84c7a3afad86d0879176175dff51
Krystian-Krol <krystian.kytek@gmail.com>
2024-01-24 12:05:46 +0100

    TiTraM linear

--- research/conditional/moe_layers/titram.py
+++ research/conditional/moe_layers/titram.py
@@ -4,8 +4,7 @@ import mamba_ssm
 import torch
 
 from lizrd.core import nn
-from lizrd.core.initialization import get_init_weight
-from lizrd.support.logging import make_histogram
+from lizrd.core.misc import Linear
 from research.conditional.utils.layer_manager import LoggingLayer
 
 
@@ -29,16 +28,17 @@ class TiTraMamba(LoggingLayer):
         self.dmodel = dmodel
         self.mamba = mamba_ssm.Mamba(d_model=self.dmodel)
         # self.mamba = DummyMamba(d_model=self.dmodel)
-        self.weight = nn.Parameter(
-            get_init_weight(
-                (self.dmodel, 8),
-                fan_in=self.dmodel * 8,
-                init_type=init_type,
-                scale=init_scale,
-            )
+
+        self.log_lookback = [0, 1, 2, 4, 8, 16, 32, 64, 128]
+        self.weight = Linear(
+            dmodel,
+            9,
+            bias=True,
+            init_type=init_type,
+            init_scale=init_scale,
         )
         self.non_neg = nn.ReLU()
-        self.log_lookback = [1, 2, 4, 8, 16, 32, 64, 128]
+        self.softmax = torch.nn.Softmax(dim=2)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -52,20 +52,14 @@ class TiTraMamba(LoggingLayer):
             .expand(batch_size, seq_len, self.dmodel)
         )
 
-        mamba_output = self.mamba.forward(x)
-        lookback_weight = torch.matmul(mamba_output, self.weight)
-        self.update_cache_for_logging("weight", lookback_weight)
+        lookback_weight = self.softmax(self.weight(x))
+        mamba_input = torch.zeros_like(x)
         for slice_idx, lookback_val in enumerate(self.log_lookback):
             lookback_idx = self.non_neg(torch.sub(lookback_limit, lookback_val))
             lookback = torch.gather(x, 1, lookback_idx)
-            mamba_output = mamba_output + lookback * torch.unsqueeze(
+            mamba_input = mamba_input + lookback * torch.unsqueeze(
                 torch.select(lookback_weight, dim=2, index=slice_idx), dim=-1
             )
-        return mamba_output
 
-    def log_heavy(self):
-        return {
-            "lookback_weight": make_histogram(
-                self.logging_cache["weight"].flatten().float()
-            ),
-        }
+        mamba_output = self.mamba.forward(mamba_input)
+        return mamba_output
