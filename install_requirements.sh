58c02a2add9c8656b5161547513a508aa8418cf2
Maciej Pióro <m@ciekpio.ro>
2024-02-06 10:52:26 +0100

    Mamba reqs

--- install_requirements.sh
+++ install_requirements.sh
@@ -5,4 +5,8 @@ cat requirements.txt
 python3 -m pip install -U pip
 python3 -m pip install -r requirements.txt
 python3 -m pip install -U multiprocess
-
+if ! command -v nvcc &> /dev/null; then
+    echo "CUDA Toolkit is not installed. Skipping installation of mamba dependencies."
+else
+    python3 -m pip install --no-build-isolation -r requirements_mamba.txt
+fi
