e344b5fcdf76d137bc79536ebff5fd2b48ba9b46
Maciej Pióro <m@ciekpio.ro>
2024-02-14 22:12:04 +0100

    Debug test

--- research/conditional/tests/test_integrated.py
+++ research/conditional/tests/test_integrated.py
@@ -15,6 +15,9 @@ class TestIntegrated(GeneralTestCase):
             "**/*.yaml"
         )
         for path in configs:
+            import os
+
+            print(list(dict(os.environ).keys()))
             print(f"Running training loop with config from {str(path)}")
             exit_code = subprocess.call(
                 ["python3", "-m", "lizrd.scripts.grid", f"--config_path={str(path)}"]
