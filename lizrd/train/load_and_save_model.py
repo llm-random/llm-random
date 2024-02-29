f214db851aeb70903d3a6ee86b1856ae1daa3dd4
kuba-krj <47078976+kuba-krj@users.noreply.github.com>
2024-02-29 16:09:21 +0100

    Update lizrd/train/load_and_save_model.py
    Co-authored-by: Michał <45643363+crewtool@users.noreply.github.com>

--- lizrd/train/load_and_save_model.py
+++ lizrd/train/load_and_save_model.py
@@ -10,10 +10,10 @@ from torch.distributed.fsdp import (
 
 
 def get_checkpoint_from_path(load_weights_path: str) -> str:
-    print(f"Loading checkpoint from disk...")
     assert os.path.exists(load_weights_path), f"Path {load_weights_path} does not exist"
+    print(f"Loading checkpoint from {load_weights_path}...")
     checkpoint = torch.load(load_weights_path)
-    print(f"Loaded from {load_weights_path}")
+    print(f"Checkpoint loaded")
     return checkpoint
 
 
