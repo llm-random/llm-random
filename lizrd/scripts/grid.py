30d26ca99611ae1c43320c0f519bbefacc671782
Kuba Krajewski <gim.jakubk@gmail.com>
2024-02-29 18:07:57 +0100

    debug

--- lizrd/scripts/grid.py
+++ lizrd/scripts/grid.py
@@ -154,7 +154,7 @@ if __name__ == "__main__":
                 f"--gres=gpu:{setup_args['n_gpus']}",
                 "--partition=plgrid-gpu-a100",
                 f"--cpus-per-gpu={setup_args['cpus_per_gpu']}",
-                "--mem-per-cpu=8G",
+                f"--mem={125*setup_args['n_gpus']}G",
                 "--account=plgsubslearnath-gpu-a100",
                 f"--job-name={job_name}",
                 f"--time={setup_args['time']}",
