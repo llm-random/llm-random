88090d92df11d6abbff31f30c53373d07e39bdb4
kuba-krj <47078976+kuba-krj@users.noreply.github.com>
2024-03-06 11:24:24 +0100

    add saving and loading models compatible with fsdp (#372)
    * add saving and loading
    
    * Minor fixes
    
    * Fixes
    
    * debug
    
    * Add blank line before if __name__ == "__main__": block
    
    * debug
    
    * Add step variable to checkpoint and load it during training
    
    * Fix optimizer loading issue and add step to checkpoint
    
    * refactor
    
    * Refactor model weight loading and saving
    
    * Update checkpoint parameter in get_model function
    
    * Refactor saving weights path in cc_train.py***
    
    ***Fix filename extension check in check_args.py
    
    * Fix save_weights_path bug in cc_train.py and check_args.py
    
    * Fix load_scaler_state() function call
    
    * Fix scaler loading issue in ConditionalTrainer
    
    * Update lizrd/train/load_and_save_model.py
    
    Co-authored-by: Michał <45643363+crewtool@users.noreply.github.com>
    
    * Refactor load_optimizer_state function
    
    * Add optional save_weights_path parameter and refactor code
    
    * Fix typo in get_save_weights_path function
    
    * Refactor prepare_save_weights_path function in cc_train.py
    
    * Fix save_weights_path return value
    
    ---------
    
    Co-authored-by: Michał <45643363+crewtool@users.noreply.github.com>

---
+++ lizrd/train/load_and_save_model.py
@@ -0,0 +1,90 @@
+from typing import Union, Optional
+import os
+
+import torch
+from torch.distributed.fsdp import (
+    FullyShardedDataParallel as FSDP,
+    FullStateDictConfig,
+    StateDictType,
+)
+
+from lizrd.support.misc import generate_random_string
+
+
+def get_checkpoint_from_path(load_weights_path: str) -> str:
+    assert os.path.exists(load_weights_path), f"Path {load_weights_path} does not exist"
+    print(f"Loading checkpoint from {load_weights_path}...")
+    checkpoint = torch.load(load_weights_path)
+    print(f"Checkpoint loaded")
+    return checkpoint
+
+
+def load_model_weights(model: torch.nn.Module, checkpoint: dict[str, torch.Tensor]):
+    print(f"Loading model weights...")
+    model.load_state_dict(checkpoint["model"], strict=False)
+    print(f"Loaded model weights")
+
+
+def load_optimizer_state(
+    optimizer: torch.optim.Optimizer,
+    checkpoint: dict[str, torch.Tensor],
+    model: Union[torch.nn.Module, FSDP],
+    rank: int,
+):
+    print(f"Loading optimizer state...")
+    if isinstance(model, FSDP):
+        full_osd = None
+        if rank == 0:
+            full_osd = checkpoint["optimizer"]
+        FSDP.scatter_full_optim_state_dict(full_osd, model)
+    else:
+        optimizer.load_state_dict(checkpoint["optimizer"])
+    print(f"Loaded optimizer state")
+
+
+def load_scaler_state(
+    scaler: torch.cuda.amp.GradScaler,
+    checkpoint: dict[str, torch.Tensor],
+):
+    scaler.load_state_dict(checkpoint["scaler"])
+
+
+def prepare_save_weights_path(path_to_dir: Optional[str]) -> str:
+    if path_to_dir is None:
+        return None
+    weights_filename = f"{generate_random_string(10)}.pt"
+    os.makedirs(path_to_dir, exist_ok=True)
+    save_weights_path = os.path.join(path_to_dir, weights_filename)
+    return save_weights_path
+
+
+def save_checkpoint(
+    model: Union[torch.nn.Module, FSDP],
+    optimizer,
+    scaler,
+    path: str,
+    rank: int,
+    step: int,
+):
+    if rank == 0 or rank is None:
+        print(f"Saving weights...")
+    if isinstance(model, FSDP):
+        save_policy = FullStateDictConfig(offload_to_cpu=True, rank0_only=True)
+        with FSDP.state_dict_type(model, StateDictType.FULL_STATE_DICT, save_policy):
+            model_state_dict = model.state_dict()
+        optimizer_state_dict = FSDP.full_optim_state_dict(model, optimizer)
+    else:
+        model_state_dict = model.state_dict()
+        optimizer_state_dict = optimizer.state_dict()
+
+    if rank == 0 or rank is None:
+        checkpoint = {
+            "model": model_state_dict,
+            "optimizer": optimizer_state_dict,
+            "step": step,
+        }
+        if scaler is not None:
+            checkpoint["scaler"] = scaler.state_dict()
+
+        torch.save(checkpoint, path)
+        print(f"Weights saved to {path} (step {step})")
