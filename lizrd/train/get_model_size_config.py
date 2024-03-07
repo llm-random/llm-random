920a99f6bdeac2f3d67065b3855704e3a33795b2
Kuba Krajewski <gim.jakubk@gmail.com>
2024-03-07 16:36:19 +0100

    Add predefined model configurations and check for conflicting arguments

--- lizrd/train/get_model_size_config.py
+++ lizrd/train/get_model_size_config.py
@@ -4,6 +4,21 @@ def get_model_size_config(predefined_config):
         dff = 1024
         n_att_heads = 4
         n_blocks = 4
+    elif predefined_config == "small":
+        dmodel = 512
+        dff = 2048
+        n_blocks = 4
+        n_att_heads = 8
+    elif predefined_config == "medium":
+        dmodel = 512
+        dff = 2048
+        n_blocks = 8
+        n_att_heads = 8
+    elif predefined_config == "base":
+        dmodel = 768
+        dff = 3072
+        n_blocks = 12
+        n_att_heads = 12
     else:
         raise ValueError(f"Unknown predefined config: {predefined_config}")
 
