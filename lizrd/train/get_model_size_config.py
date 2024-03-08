84d228690ff9264a7b81a179aad462a09e01a2a1
Kuba Krajewski <gim.jakubk@gmail.com>
2024-03-08 16:11:29 +0100

    Add predefined model configurations

--- lizrd/train/get_model_size_config.py
+++ lizrd/train/get_model_size_config.py
@@ -19,6 +19,46 @@ def get_model_size_config(predefined_config):
         dff = 3072
         n_blocks = 12
         n_att_heads = 12
+    elif predefined_config == "base_bigger":
+        dmodel = 1024
+        dff = 4096
+        n_blocks = 16
+        n_att_heads = 16
+    elif predefined_config == "flag":
+        dmodel = 896
+        dff = 3584
+        n_blocks = 14
+        n_att_heads = 14
+    elif predefined_config == "flag_big":
+        dmodel = 1280
+        dff = 5120
+        n_blocks = 20
+        n_att_heads = 20
+    elif predefined_config == "clark_medium_small":
+        dmodel = 1536
+        dff = 6144
+        n_blocks = 12
+        n_att_heads = 12
+    elif predefined_config == "big_between":
+        dmodel = 1792
+        dff = 7168
+        n_blocks = 14
+        n_att_heads = 14
+    elif predefined_config == "clark_medium":
+        dmodel = 2048
+        dff = 8192
+        n_blocks = 16
+        n_att_heads = 16
+    elif predefined_config == "clark_big_smaller":
+        dmodel = 1792
+        dff = 7168
+        n_blocks = 23
+        n_att_heads = 14
+    elif predefined_config == "clark_big":
+        dmodel = 2048
+        dff = 8192
+        n_blocks = 24
+        n_att_heads = 16
     else:
         raise ValueError(f"Unknown predefined config: {predefined_config}")
 
