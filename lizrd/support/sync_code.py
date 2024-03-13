0692de73ff73178cf70a45e6dc5035af7980ccf1
Michał <45643363+crewtool@users.noreply.github.com>
2024-03-13 13:09:06 +0100

    Split version and sync (#392)
    split sync versioning

--- lizrd/support/sync_code.py
+++ lizrd/support/sync_code.py
@@ -9,7 +9,6 @@ from argparse import ArgumentParser
 from git import Repo
 import paramiko.ssh_exception
 
-from lizrd.support.code_versioning import version_code
 from lizrd.support.misc import generate_random_string
 
 _SSH_HOSTS_TO_PASSPHRASES = {}
@@ -140,4 +139,3 @@ if __name__ == "__main__":
     )
     with open("/tmp/git_branch.txt", "w") as f:
         f.write(name_for_branch)
-    version_code(name_for_branch)
