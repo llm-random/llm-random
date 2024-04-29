9fd7207beefbc7a7fdfaec577c0e3881b384e35f
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2024-04-29 13:14:53 +0200

    no iterable

--- performance_datasets.py
+++ performance_datasets.py
@@ -4,10 +4,8 @@ from datasets import load_from_disk
 # from random import randint
 
 load_start = time()
-dataset = (
-    load_from_disk("/net/tscratch/people/plgkciebiera/datasets/c4/train")
-    .to_iterable_dataset()
-    .shuffle(seed=42)
+dataset = load_from_disk("/net/tscratch/people/plgkciebiera/datasets/c4/train").shuffle(
+    seed=42
 )
 load_time = time() - load_start
 print(f"Load time: {load_time:.3f}s")
