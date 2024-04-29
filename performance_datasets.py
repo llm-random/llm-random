bb61c4470eb619c7a9cd951fad3884d3d484d0ad
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2024-04-29 12:53:23 +0200

    enumerate_dataset

--- performance_datasets.py
+++ performance_datasets.py
@@ -7,11 +7,12 @@ dataset = load_from_disk("/net/tscratch/people/plgkciebiera/datasets/c4/train")
 load_time = time() - load_start
 print(f"Load time: {load_time:.3f}s")
 times = []
-for i in range(20):
-    curr_time = time()
+curr_time = time()
+for i, dataset in enumerate(dataset):
     document = dataset[randint(0, len(dataset))]["text"]
     print(document[:100])
     read_time = time() - curr_time
     print(f"Time: {read_time}s")
     times.append(read_time)
+    curr_time = time()
 print(f"Average time: {sum(times) / len(times):.3f}s")
