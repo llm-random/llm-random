8e70ae27c034c69f5bdf469417a72b2a65396295
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2024-05-20 16:52:26 +0200

    check load times on clusters

--- performance_datasets.py
+++ performance_datasets.py
@@ -1,27 +1,21 @@
 from time import time
-import dask.dataframe
 
-# from datasets import load_from_disk
-import dask
+from datasets import load_from_disk
 
 from random import randint
 
 load_start = time()
-dataset = dask.dataframe.read_parquet(
+dataset = load_from_disk(
     "/net/tscratch/people/plgkciebiera/datasets/c4/train",
-    parquet_file_extension=(".arrow"),
-    engine="pyarrow",
 )
 load_time = time() - load_start
 print(f"Load time: {load_time:.3f}s")
 times = []
 curr_time = time()
 for i in range(100):
-    # print(document[:100])
     document = dataset[randint(0, len(dataset) - 1)]
     document = document["text"]
     read_time = time() - curr_time
-    # print(f"Time: {read_time}s")
     times.append(read_time)
     if i == 100:
         break
