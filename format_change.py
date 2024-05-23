ef7387710278adacfd335f1a0a759877571664e2
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2024-05-23 14:33:53 +0200

    Change dataset format

---
+++ format_change.py
@@ -0,0 +1,7 @@
+from datasets import load_from_disk
+
+
+dataset = load_from_disk(
+    "/net/tscratch/people/plgkciebiera/datasets/c4/validation",
+)
+dataset.to_parquet("/net/tscratch/people/plgkciebiera/datasets2/c4/validation")
