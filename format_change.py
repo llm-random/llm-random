11aa02134b23470ded2918e7b42922dd157c5f7d
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2024-05-23 14:41:39 +0200

    typo

--- format_change.py
+++ format_change.py
@@ -4,4 +4,6 @@ from datasets import load_from_disk
 dataset = load_from_disk(
     "/net/tscratch/people/plgkciebiera/datasets/c4/validation",
 )
-dataset.to_parquet("/net/tscratch/people/plgkciebiera/datasets2/c4/validation")
+dataset.to_parquet(
+    "/net/tscratch/people/plgkciebiera/datasets2/c4/validation/validation.parquet"
+)
