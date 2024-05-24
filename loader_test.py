9eb08b2f4dbfab500a84b524ff112db94902b17e
Kamil Ciebiera <kamil.ciebiera@gmail.com>
2024-05-24 16:16:42 +0200

    test loader

---
+++ loader_test.py
@@ -0,0 +1,20 @@
+from research.datasets import get_processed_dataset
+
+loader = get_processed_dataset(
+    batch_size=4,
+    sequence_length=16,
+    device="cpu",
+    num_workers=1,
+    seed=42,
+    model_type="gpt",
+    dataset_type="c4",
+    use_dummy_dataset=False,
+    dataset_path="/net/tscratch/people/plgkciebiera/datasets/c4/train",
+    dataset_split="train",
+    num_gpus=1,
+)
+
+print("GETTING BATCH")
+batch = loader.get_batch()
+print("GOT BATCH")
+print(batch)
