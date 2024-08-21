ad16b97792d8a046b6c8605258bbeda6f08ad186
m1kush <m1kush@disroot.org>
2024-08-21 13:56:55 +0200

    Versioning code

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m lizrd.grid --config_path=configs/experiments/multinode/multinode_1x1.yaml --git_branch=multinode_1x1_2024-08-20_21-06-10 --skip_copy_code
+python3 -m lizrd.grid --config_path=configs/experiments/multinode/multinode_1x1.yaml --git_branch=multinode_1x1_2024-08-21_13-56-55 --skip_copy_code
