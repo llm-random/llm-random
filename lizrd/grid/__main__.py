e8fa8895f6deb3ffbdebdab8e1c06e9711bf7b51
Michał Krutul <crewtoolcompany@gmail.com>
2024-09-05 14:50:03 +0000

    add debug code

--- lizrd/grid/__main__.py
+++ lizrd/grid/__main__.py
@@ -8,8 +8,19 @@ import os
 from time import sleep
 from lizrd.grid.grid import create_subprocess_args
 
+from lizrd.core import misc
 from lizrd.grid.infrastructure import LocalBackend, get_machine_backend
 import subprocess
+
+
+import random
+import socket
+
+import torch.multiprocessing as mp
+
+from research.token_reduction.utils.argparse import introduce_parser_arguments
+from research.token_reduction.utils.runner import assert_n_gpus
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -55,5 +66,27 @@ if __name__ == "__main__":
         runner_main_function, runner_params = experiments[0]
         # We run the experiment directly, not through a grid entrypoint script
         # because we want to be able to debug it
-        runner_main_function(None, runner_params=runner_params)
+        misc.print_available_gpus()
+        parser = argparse.ArgumentParser()
+        introduce_parser_arguments(parser)
+        args = parser.parse_args(runner_params)
+        assert_n_gpus(args.n_gpus)
+        if args.data_seed < 0:
+            args.data_seed = random.randint(0, 10000000)
+
+        if args.ddp_enabled or args.fsdp_enabled:
+            random.seed(args.data_seed)
+            data_seeds = [random.randint(0, 10000000) for _ in range(args.n_gpus)]
+
+            # find free port
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind(("", 0))
+                port = str(s.getsockname()[1])
+            mp.spawn(
+                runner_main_function,
+                args=[data_seeds, port, args],
+                nprocs=args.n_gpus,
+            )
+        else:
+            runner_main_function(None, runner_params=runner_params)
         exit(0)
