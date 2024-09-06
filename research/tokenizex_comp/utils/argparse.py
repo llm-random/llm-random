aac8dfc25b604741a167f65c11d8cad749b3e2e4
madragonse <madragonse@gmail.com>
2024-09-06 16:17:44 +0200

    fix new arg req

--- research/tokenizex_comp/utils/argparse.py
+++ research/tokenizex_comp/utils/argparse.py
@@ -29,6 +29,7 @@ def introduce_parser_arguments(
     parser.add_argument("--learning_rate", type=float, required=True)
     parser.add_argument("--scheduler", type=str, required=True)
     parser.add_argument("--final_lr_step", type=int, required=False)
+    parser.add_argument("--lr_warmup_percent", type=float, required=False)
     parser.add_argument("--final_lr_fraction", type=float, required=False)
     parser.add_argument(
         "--init_type",
