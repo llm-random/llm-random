2a1d95a3d715222d8b474e5d04f7208a49adb8a8
Maciej Pióro <m@ciekpio.ro>
2024-10-07 16:13:44 +0200

    WIP

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# use either `apptainer` or `singularity` - should be mostly compatible
 COMMAND=$(command -v apptainer >/dev/null && echo apptainer || echo singularity)
-# $COMMAND build --fakeroot sparsity-base.sif sparsity-base.def
-$COMMAND build --fakeroot sparsity_`date +'%Y.%m.%d_%H.%M.%S'`.sif sparsity-head.def
+$COMMAND build --fakeroot sparsity-base.sif sparsity-base.def
+# $COMMAND build --fakeroot sparsity_`date +'%Y.%m.%d_%H.%M.%S'`.sif sparsity-head.def
