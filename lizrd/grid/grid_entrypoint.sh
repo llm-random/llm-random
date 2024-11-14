eb259d33aa3911da911877f012532d681c007c55
madragonse <madragonse@gmail.com>
2024-11-14 12:30:25 +0100

    dev venv

--- lizrd/grid/grid_entrypoint.sh
+++ lizrd/grid/grid_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 module CUDA/11.7.0
+source /net/pr2/projects/plgrid/plggllmeffi/plgmstefaniak/venv/bin/activate
 echo "Will run the following command:"
 echo "$@"
 echo "==============================="
--- lizrd/grid/infrastructure.py
+++ lizrd/grid/infrastructure.py
@@ -129,13 +129,13 @@ class AthenaBackend(MachineBackend):
             f"--job-name={training_args['name']}",
             f"--time={setup_args['time']}",
             f"{setup_args['grid_entrypoint']}",
-            "singularity",
-            "run",
-            "--bind=/net:/net",
-            *singularity_env_arguments,
-            make_singularity_mount_paths(setup_args, training_args),
-            "--nv",
-            setup_args["singularity_image"],
+            # "singularity", # dev TODO temporary solution
+            # "run",
+            # "--bind=/net:/net",
+            # *singularity_env_arguments,
+            # make_singularity_mount_paths(setup_args, training_args),
+            # "--nv",
+            # setup_args["singularity_image"],
             *self.get_runner_command(setup_args["runner"], runner_params),
         ]
 
