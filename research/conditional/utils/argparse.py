105da38b458669a47933a6b4f8d25aa4f7cb3e3d
kuba-krj <47078976+kuba-krj@users.noreply.github.com>
2024-11-21 15:05:27 +0100

    Implement proper decay fraction (#488)
    * Implement proper decay fraction
    
    * fix
    
    * fix param name
    
    * Fix OOMs
    
    * fix typo
    
    * fix a bug
    
    ---------
    
    Co-authored-by: Maciej Pióro <m@ciekpio.ro>
    Co-authored-by: Maciej Pióro <70006947+maciek-pioro@users.noreply.github.com>

--- research/conditional/utils/argparse.py
+++ research/conditional/utils/argparse.py
@@ -117,6 +117,12 @@ def introduce_parser_arguments(
         "--lr_warmup_tokens", type=float, default=None, help="in bilions of tokens"
     )
     parser.add_argument("--lr_trapezoidal_decay_fraction", type=float, default=0.20)
+    parser.add_argument(
+        "--lr_trapezoidal_decay_fraction_unit",
+        type=str,
+        default="tokens",
+        choices=["tokens", "steps"],
+    )
     parser.add_argument("--lr_decay_interval", type=int, default=0)
     parser.add_argument(
         "--batch_size_rampup_transition_points",
