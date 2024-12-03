ccb7655f4dd5b77f87a599ed5ee09d31f168c934
Maciej Pióro <m@ciekpio.ro>
2024-12-03 12:02:10 +0100

    Move to custom backends

--- research/attention_moe/entrypoints/writer.sh
+++ research/attention_moe/entrypoints/writer.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-module CUDA/11.7.0
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate momqa
+which python3
+pip freeze
 echo "Will run the following command:"
 echo "$@"
 echo "==============================="
