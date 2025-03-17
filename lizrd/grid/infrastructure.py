18120eccc418ffec391d25045595d2e955b5c41d
Janek <jan.malasnicki@gmail.com>
2025-03-17 11:14:47 +0100

    updated helios account

--- lizrd/grid/infrastructure.py
+++ lizrd/grid/infrastructure.py
@@ -215,7 +215,7 @@ class HeliosBackend(MachineBackend):
             f"--array=0-{n_consecutive-1}%1",
             "--partition=plgrid-gpu-gh200",
             "--exclusive",  # request all non-gpu resources on node
-            "--account=plgllmefficont-gpu-gh200",
+            "--account=plgllmefficont2-gpu-gh200",
             f"--job-name={training_args['name']}",
             f"--time={setup_args['time']}",
             f"{setup_args['grid_entrypoint']}",
