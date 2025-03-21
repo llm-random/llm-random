9ad7ec43435a8118df5e126015900c5786796aef
Janek <jan.malasnicki@gmail.com>
2025-03-21 17:29:34 +0100

    tiny code cleanup

--- nano/token_reduction/tests/test_mtp.py
+++ nano/token_reduction/tests/test_mtp.py
@@ -17,55 +17,48 @@ from model import (
 )
 
 
-# class TestTrainerMTP(unittest.TestCase):
-#     def test_mtp(self):
-#         with initialize(version_base=None, config_path="configs"):
-#             cfg = compose(config_name="test_mtp", overrides=[])
-@hydra.main(version_base=None, config_path="configs", config_name="test_mtp")
-def tmp(cfg):
-    setup_enviroment()
-    metric_logger = get_metric_logger(
-        metric_logger_config=instantiate(cfg.metric_logger, _convert_="all"),
-    )
+class TestTrainerMTP(unittest.TestCase):
+    def test_mtp(self):
+        with initialize(version_base=None, config_path="configs"):
+            cfg = compose(config_name="test_mtp", overrides=[])
 
-    model = instantiate(cfg.model, _convert_="all")
+        setup_enviroment()
+        metric_logger = get_metric_logger(
+            metric_logger_config=instantiate(cfg.metric_logger, _convert_="all"),
+        )
 
-    optimizer = torch.optim.AdamW(
-        model.parameters(),
-        lr=cfg.training.learning_rate,
-        weight_decay=cfg.training.weight_decay,
-    )
+        model = instantiate(cfg.model, _convert_="all")
 
-    scheduler = instantiate(cfg.training.scheduler)(optimizer=optimizer)
+        optimizer = torch.optim.AdamW(
+            model.parameters(),
+            lr=cfg.training.learning_rate,
+            weight_decay=cfg.training.weight_decay,
+        )
 
-    dataloaders_factory = instantiate(cfg.dataloaders_factory)
-    train_dataloader, eval_dataloader = dataloaders_factory()
+        scheduler = instantiate(cfg.training.scheduler)(optimizer=optimizer)
 
-    training_state = {"next_step": 0, "run_id": None, "processed_tokens": 0}
+        dataloaders_factory = instantiate(cfg.dataloaders_factory)
+        train_dataloader, eval_dataloader = dataloaders_factory()
 
-    trainer_factory = instantiate(cfg.trainer_factory)
-    # print(f'cfg.trainer_factory:\n{cfg.trainer_factory}')
-    # print(f'trainer_factory:\n{type(trainer_factory)}')
-    # print(f'model: {type(model)}')
-    mtp_trainer = trainer_factory(
-        model=model,
-        optimizer=optimizer,
-        scheduler=scheduler,
-        training_state=training_state,
-        train_dataloader=train_dataloader,
-        eval_dataloader=eval_dataloader,
-        metric_logger=metric_logger,
-    )
-    # print(f'mtp_trainer: {type(mtp_trainer)}')
-    print("ok")
-    mtp_trainer.train()
+        training_state = {"next_step": 0, "run_id": None, "processed_tokens": 0}
 
-
-def test_preprocess_input_mtp(self):
-    test_batch = torch.rand(size=(3, 7))
+        trainer_factory = instantiate(cfg.trainer_factory)
+        # print(f'cfg.trainer_factory:\n{cfg.trainer_factory}')
+        # print(f'trainer_factory:\n{type(trainer_factory)}')
+        # print(f'model: {type(model)}')
+        mtp_trainer = trainer_factory(
+            model=model,
+            optimizer=optimizer,
+            scheduler=scheduler,
+            training_state=training_state,
+            train_dataloader=train_dataloader,
+            eval_dataloader=eval_dataloader,
+            metric_logger=metric_logger,
+        )
+        # print(f'mtp_trainer: {type(mtp_trainer)}')
+        print("ok")
+        mtp_trainer.train()
 
 
-# t = TestTrainerMTP()
-# t.test_mtp()
-
-tmp()
+if __name__ == "__main__":
+    unittest.main()
