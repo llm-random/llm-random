c98583fb96e47d6263db70c9a343fafb253f6934
j321m <97523346+j321m@users.noreply.github.com>
2025-03-28 15:58:38 +0100

    merge MTP to nano (#513)
    * mtp tester debug
    
    * mtp tester debug
    
    * mtp fixed some bugs
    
    * first working mtp
    
    * black reformatting
    
    * tiny code cleanup
    
    * MTP runs on entropy
    
    * MTP loss logging
    
    * fix trainer factory partial
    
    * train loop steps + 1
    
    * train mtp + eval seed configs
    
    * black formatting
    
    * added loss/100
    
    * k 16 configs and k 8 mtp 1
    
    * deterministic test_mtp
    
    * black formatting
    
    * nano mtp merge fix
    
    * fixed bfloat16 eval loss

--- nano/model.py
+++ nano/model.py
@@ -151,7 +151,7 @@ def run(cfg):
     load_checkpoint(
         cfg.checkpoint_config, model, optimizer, scheduler, train_dataloader
     )
-    trainer_factory = instantiate(cfg.trainer_factory, _partial_=True)
+    trainer_factory = instantiate(cfg.trainer_factory)
     trainer_factory(
         model=model,
         optimizer=optimizer,
@@ -449,7 +449,7 @@ class RMSNorm(nn.Module):
         self.b = nn.Parameter(torch.zeros(dmodel))
 
     def forward(self, x):
-        norm = torch.mean(x**2, dim=-1, keepdim=True)
+        norm = torch.mean(x ** 2, dim=-1, keepdim=True)
         x = x * torch.rsqrt(norm + self.eps)
         return x * self.g + self.b
 
@@ -598,7 +598,6 @@ class PositionalEmbedding(nn.Module):
 
 
 class TransformerBlock(nn.Module):
-
     def __init__(
         self,
         common,
@@ -633,7 +632,6 @@ class TransformerBlock(nn.Module):
 
 
 class TransformerTower(nn.Module):
-
     def __init__(
         self,
         common: Common,
@@ -717,7 +715,6 @@ class PredictionHead(Linear):
 
 
 class LLM(nn.Module):
-
     def __init__(
         self,
         embedding,
@@ -927,7 +924,6 @@ class MetricLogger(ABC):
 
 
 class MetricAccumulator:
-
     def __init__(self, calculate_fn, metrics):
         self.acc_dict = {}
         self.calculate_fn = calculate_fn
@@ -954,7 +950,6 @@ class DummyLogger(MetricLogger):
 
 
 class NeptuneLogger(MetricLogger):
-
     def __init__(self, run, rank, config=None):
         super().__init__(config)
         self.run = run
@@ -966,7 +961,6 @@ class NeptuneLogger(MetricLogger):
 
 
 class StdoutLogger(MetricLogger):
-
     def __init__(self, config=None):
         super().__init__(config)
         self.rank = os.environ.get("RANK", 0)
@@ -1149,6 +1143,7 @@ class Trainer:
         self.processed_tokens = self.training_state["processed_tokens"]
         self.start_step = self.training_state["next_step"]
         self.device = next(self.model.parameters()).device
+        self.loss_interval_100 = 0.0
 
     @property
     def _should_evaluate(self) -> bool:
@@ -1197,7 +1192,6 @@ class Trainer:
         return input_ids, target_ids
 
     def calculate_loss(self, batch):
-
         def _hack_for_python_garbage_collection(input_ids, target_ids):
             """we want to have no reference to model output while backpropagating to allow torch to free memory,
             so we wrap loss calculation in a function"""
@@ -1537,7 +1531,6 @@ def wrap_model(model, fsdp_config):
 
 
 class TrainingState(Stateful):
-
     def __init__(self, model, optimizer, scheduler, train_dataloader):
         self.model = model
         self.optimizer = optimizer
