61c86ef671e8e88743facae6a8b365e79005b89c
Janek <jan.malasnicki@gmail.com>
2025-05-09 12:17:26 +0200

    fixed mtp meta testing

--- nano/token_reduction/tests/test_mtp.py
+++ nano/token_reduction/tests/test_mtp.py
@@ -1,60 +1,50 @@
 import unittest
-import hydra
 from unittest.mock import patch
 import torch
 
 from hydra import initialize, compose
 from hydra.utils import instantiate
 
+
 from token_reduction.model import (
-    TrainerMTP,
     LLM_MTP,
+    TrainerMTP,
 )
+
 from model import (
-    StdoutLogger,
-    setup_enviroment,
-    get_metric_logger,
+    RecorderLogger,
+    load_training_state,
+    run,
 )
 
 
-class TestTrainerMTP(unittest.TestCase):
-    def test_mtp(self):
+class TestSimpleRun(unittest.TestCase):
+    @patch("model.get_metric_logger", return_value=RecorderLogger())
+    def test_simple_mtp_deepseek(self, get_metric_logger):
+        target_losses_dropping = [
+            (10.940040588378906, 0),
+            (10.923925399780273, 1),
+            (10.917631149291992, 2),
+            (10.922800064086914, 3),
+            (10.90736198425293, 4),
+            (10.912927627563477, 5),
+            (10.859469413757324, 6),
+            (10.839786529541016, 7),
+            (10.807689666748047, 8),
+            (10.827632904052734, 9),
+        ]
         with initialize(version_base=None, config_path="configs"):
             cfg = compose(config_name="test_mtp", overrides=[])
+            training_state = load_training_state(cfg.checkpoint_config)
+            metric_logger = get_metric_logger(
+                metric_logger_config=instantiate(cfg.metric_logger, _convert_="all"),
+                neptune_run_id=training_state["run_id"],
+            )
+            run(cfg)
 
-        setup_enviroment()
-        metric_logger = get_metric_logger(
-            metric_logger_config=instantiate(cfg.metric_logger, _convert_="all"),
-        )
-        torch.manual_seed(cfg.training.seed)
-        device = torch.device("cpu")
-
-        model = instantiate(cfg.model, _convert_="all").to(device)
-
-        optimizer = torch.optim.AdamW(
-            model.parameters(),
-            lr=cfg.training.learning_rate,
-            weight_decay=cfg.training.weight_decay,
-        )
-
-        scheduler = instantiate(cfg.training.scheduler)(optimizer=optimizer)
-
-        dataloaders_factory = instantiate(cfg.dataloaders_factory)
-        train_dataloader, eval_dataloader = dataloaders_factory()
-
-        training_state = {"next_step": 0, "run_id": None, "processed_tokens": 0}
-
-        trainer_factory = instantiate(cfg.trainer_factory)
-        mtp_trainer = trainer_factory(
-            model=model,
-            optimizer=optimizer,
-            scheduler=scheduler,
-            training_state=training_state,
-            train_dataloader=train_dataloader,
-            eval_dataloader=eval_dataloader,
-            metric_logger=metric_logger,
-        )
-        mtp_trainer.train()
+            self.assertListEqual(
+                target_losses_dropping, metric_logger.data["steps/train/loss"]
+            )
 
 
 if __name__ == "__main__":
